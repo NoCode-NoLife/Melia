aecbf36ed761b77176658d933852eb0cc2783ca0
exec <exec@mabinoger.de>
2015-08-18 02:15:58 +0200

    Added a new reference struct

---
+++ src/Shared/Const/Structs/CB_LOGIN.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Melia.Shared.Const.Structs
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+	public struct CB_LOGIN
+	{
+		//public short Op;
+		//public int Index;
+		//public int Checksum;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 33)]
+		public string AccountName;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+		public string Password;
+		public byte UnkByte1;
+		public byte UnkByte2;
+		public int Ip;
+	}
+}
