4bf992ecc029466763bf31a46ab68212830cc27d
exec <exec@mabinoger.de>
2015-08-31 17:54:44 +0200

    Moved dialog doc from src to doc folder

--- src/ChannelServer/Network/ChannelPacketSender.cs
+++ src/ChannelServer/Network/ChannelPacketSender.cs
@@ -816,59 +816,13 @@ namespace Melia.Channel.Network
 
 
 
-		//	//"SIAUL_WEST_CAMP_MANAGER_basic1",
-		//	//"{img minimap_1_MAIN 16 16}@dicID_^*$QUEST_LV_0100_20150717_007623$*^",
-		//	//"@dicID_^*$ETC_20150317_004891$*^",
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//null,
-		//	//"!@#ScpArgMsg$Auto_JongLyo#@!",
-		//};
 
-		//packet = new Packet(Op.ZC_DIALOG_SELECT);
 
-		//packet.PutInt(handle);
-		//packet.PutShort(strings.Length);
-		//foreach (var str in strings)
-		//	packet.PutStringWithLength(str);
 
-		//conn.Send(packet);
 
-		// {/} reset
-		// {np} new paragraph?
-		// {nl} new line
-		// {s35} size
-		// {img F3 40 40} image <key> <width?> <height?>
-		// !@#ScpArgMsg$...#@! clientmessage.xml reference? (... = key?)
-		// {b} bold
-		// {ol} outline
-		// {ds} drop shadow
 
-		//packet = new Packet(Op.ZC_DIALOG_OK);
-		//packet.PutInt(0); // handle?
-		//packet.PutString("SIAUL_WEST_RESIDENT1_basic2");
-		//conn.Send(packet);
 
-		//packet = new Packet(Op.ZC_DIALOG_NEXT);
-		//packet.PutInt(0); // handle?
-		//packet.PutString("SIAUL_WEST_RESIDENT1_basic2");
-		//conn.Send(packet);
 
-		//packet = new Packet(Op.ZC_DIALOG_STRINGINPUT);
-		//packet.PutInt(0); // handle?
-		//packet.PutString("SIAUL_WEST_RESIDENT1_basic2");
-		//conn.Send(packet);
-
-		//packet = new Packet(Op.ZC_DIALOG_NUMBERRANGE);
-		//packet.PutInt(0); // handle?
-		//packet.PutString("SIAUL_WEST_RESIDENT1_basic2");
-		//conn.Send(packet);
 
 		public static void ZC_DIALOG_OK(ChannelConnection conn, string msg)
 		{
