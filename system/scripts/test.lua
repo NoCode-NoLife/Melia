8b1c9fc23594edd618df2e035bb40c3d5d59b270
exec <exec@mabinoger.de>
2015-09-02 13:05:01 +0200

    Removed obsolete close calls from test script

--- system/scripts/test.lua
+++ system/scripts/test.lua
@@ -6,16 +6,11 @@ function testnpc()
 	local name2 = "THAT Npc*@*"
 	
 	select(name.."Well, hello there, {pcname}, how are you?", "Who are you?")
-	close()
 	msg(name.."Who {b}I{/} am?{np}I am {s30}{#ffffff}{ol}THAT Npc{/}{s20}{#111111} of course, the one and only! Hah!")
 	select(name2.."The only NPC Melia will ever have or need!!", "Uhm...")
-	close()
 	if select(name2.."Wait, what? I'm not gonna be the only one? I'm just a... test?", "Yea...", "Cancel") == 2 then
-		close()
 		return
 	end
-
-	close()
 	msg(name2.."I... I understand... I shouldn't have assumed...")
 	msg(name2.."If you'll excuse me... *sniff*")
 	msg(name2.."But...")
