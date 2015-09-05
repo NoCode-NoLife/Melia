7fac085fe8421935f608ffa3fbb5bc4e6ec9d870
exec <exec@mabinoger.de>
2015-09-05 17:28:15 +0200

    Renamed test.lua

--- system/scripts/f_siauliai_west.lua
+++ system/scripts/f_siauliai_west.lua
@@ -1,9 +1,14 @@
-
-addnpc(57223, "Some Npc", "f_siauliai_west", -867, 322, 151, 135, "testnpc")
+----- Melia Script ----------------------------------------------------------
+-- West Siauliai Woods
+----- Description -----------------------------------------------------------
+-- NPCs and warps on f_siauliai_west.
+-----------------------------------------------------------------------------
 
 -- West Siauliai <> Klaipeda
 addwarp("WS_SIAULST1_KLAPEDA", 22, "f_siauliai_west", 1691, 210, -755, "c_Klaipe", -136, -1, -1035)
 addwarp("WS_KLAPEDA_SIAULST1", -45, "c_Klaipe", -209, -1, -1189, "f_siauliai_west", 1591, 210, -739)
+
+addnpc(57223, "Some Npc", "f_siauliai_west", -867, 322, 151, 135, "testnpc")
 
 function testnpc()
 	local name = "???*@*"
