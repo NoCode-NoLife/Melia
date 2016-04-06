04db6e556a60af7b013cf0f8a30dd65276505e52
Hirasu <Hirasukun@hotmail.com>
2016-04-06 12:58:50 +0200

    Deleted duplicated folders

--- system/scripts/field/f_pilgrimroad_49.lua
+++
@@ -1,4 +0,0 @@
-addnpc(155035, "QUEST_UNUSED_20151001_000478", "f_pilgrimroad_49", 1423.411, 348.6612, -424.8268, -65, "DummyCall")
-addnpc(152065, "QUEST_LV_0100_20151001_008362", "f_pilgrimroad_49", -265.8982, 347.7849, 1170.751, -11, "DummyCall")
-addnpc(155038, "QUEST_LV_0100_20151001_008373", "f_pilgrimroad_49", -1192.19, 201.7679, -2044.803, -13, "DummyCall")
-addnpc(147392, "ETC_20150317_009100", "f_pilgrimroad_49", -169.54, 294.01, -1567.06, 180, "DummyCall")
