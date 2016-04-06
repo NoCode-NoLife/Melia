7842bcc520dfcef9758f1d075a2985213273fed9
Hirasu <Hirasukun@hotmail.com>
2016-04-06 13:14:46 +0200

    Add Space / Removed empty script files

--- system/scripts/npc/dungeon/mission_groundtower_1.lua
+++ system/scripts/npc/dungeon/mission_groundtower_1.lua
@@ -1,1 +1,1 @@
-addnpc(154013,"QUEST_20160224_003701","mission_groundtower_1",3079.993,266.48,-6288.347,23,"DummyCall")
+addnpc(154013, "QUEST_20160224_003701", "mission_groundtower_1", 3079.993, 266.48, -6288.347, 23, "DummyCall")
