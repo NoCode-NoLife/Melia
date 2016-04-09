9aabc0e7c83f6cb72d340ed83d454807664cf333
exec <exec@mabinoger.de>
2016-04-09 02:08:09 +0200

    Fixed ZC_SET_POS and added template
    There was a byte missing.

--- src/ChannelServer/Network/ChannelPacketSender.cs
+++ src/ChannelServer/Network/ChannelPacketSender.cs
@@ -789,6 +789,7 @@ namespace Melia.Channel.Network
 			packet.PutFloat(character.Position.X);
 			packet.PutFloat(character.Position.Y);
 			packet.PutFloat(character.Position.Z);
+			packet.PutByte(0);
 
 			character.Map.Broadcast(packet, character);
 		}
