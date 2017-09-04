cb077efc9b46c45babc5fd48190cf70b38e9f0d4
celophi <celophi@tuta.io>
2017-09-04 11:16:29 -0700

    Moved constant barrack messages into separate class.

--- src/LoginServer/Network/LoginPacketHandler.cs
+++ src/LoginServer/Network/LoginPacketHandler.cs
@@ -187,7 +187,7 @@ namespace Melia.Login.Network
 
 			Send.BC_BARRACKNAME_CHANGE(conn, TeamNameChangeResult.Okay);
 			Send.BC_ACCOUNT_PROP(conn, conn.Account);
-			Send.BC_NORMAL_Run(conn, "THEMA_BUY_SUCCESS");
+			Send.BC_NORMAL_Run(conn, BarrackMessage.THEMA_BUY_SUCCESS);
 		}
 
 		/// <summary>
@@ -405,7 +405,7 @@ namespace Melia.Login.Network
 			conn.Account.SelectedBarrack = newMapId;
 
 			Send.BC_ACCOUNT_PROP(conn, conn.Account);
-			Send.BC_NORMAL_Run(conn, "THEMA_BUY_SUCCESS");
+			Send.BC_NORMAL_Run(conn, BarrackMessage.THEMA_BUY_SUCCESS);
 		}
 
 		/// <summary>
