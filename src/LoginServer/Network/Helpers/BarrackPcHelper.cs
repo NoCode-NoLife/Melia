8370a9289340ff1249084e604da7e506d94afc01
exec <exec@dragonbyte.de>
2017-09-05 12:08:32 +0200

    Fixed BC_COMMANDER_LIST
    I though there were two more new bytes, and that the list wasn't a list
    anymore, but the new bytes only shifted everything a little, making it
    look like the length of the list became two new bytes.

--- src/LoginServer/Network/Helpers/BarrackPcHelper.cs
+++ src/LoginServer/Network/Helpers/BarrackPcHelper.cs
@@ -21,17 +21,16 @@ namespace Melia.Login.Network.Helpers
 			packet.PutByte(1);
 			packet.PutByte(1);
 			packet.PutByte(1);
-			packet.PutByte(1);
-			packet.PutByte(0);
 
 			// Job history?
 			// While this short existed in iCBT1, it might not have
 			// been used, couldn't find a log.
 			// Example: A Mage that switched to Pyromancer has two
 			//   elements in this list, 2001 and 2002.
-			// In i170175 this short seems to be the current job now,
-			// and there's no sign of a list.
-			packet.PutShort(0); // count
+			packet.PutShort(1); // count
+			{ // foreach
+				packet.PutShort((short)pc.Job);
+			}
 
 			// [i11025 (2016-02-26)] ?
 			{
