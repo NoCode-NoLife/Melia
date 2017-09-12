ea351cf45d17210dab1b3e9babfbd6f7711a4d38
exec <exec@dragonbyte.de>
2017-09-12 11:03:40 +0200

    Added Properties
    For future use in objects and packets.

---
+++ src/Shared/World/ObjectProperties/Properties.cs
@@ -0,0 +1,127 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Melia.Shared.Const;
+
+namespace Melia.Shared.World.ObjectProperties
+{
+	public class Properties
+	{
+		private Dictionary<int, IProperty> _properties = new Dictionary<int, IProperty>();
+
+		/// <summary>
+		/// Returns the byte size of all properties, as they would take
+		/// in a packet.
+		/// </summary>
+		/// XXX: Could be cached if performance is a problem.
+		public int Size { get { lock (_properties) return _properties.Values.Sum(a => a.Size); } }
+
+		/// <summary>
+		/// Returns the property with the given property id,
+		/// or null if it doesn't exist.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <returns></returns>
+		public IProperty Get(int propertyId)
+		{
+			lock (_properties)
+			{
+				if (!_properties.TryGetValue(propertyId, out var property))
+					return null;
+
+				return property;
+			}
+		}
+
+		/// <summary>
+		/// Returns list of all set properties.
+		/// </summary>
+		/// <returns></returns>
+		public IProperty[] GetAll()
+		{
+			lock (_properties)
+				return _properties.Values.ToArray();
+		}
+
+		/// <summary>
+		/// Returns list of all set properties.
+		/// </summary>
+		/// <returns></returns>
+		public IProperty[] GetAll(params int[] propertyIds)
+		{
+			lock (_properties)
+				return _properties.Values.Where(a => propertyIds.Contains(a.Id)).ToArray();
+		}
+
+		/// <summary>
+		/// Sets the given propery's value.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <param name="value"></param>
+		public void Set(int propertyId, float value)
+		{
+			if (GetPropertyType(propertyId) != PropertyType.Float)
+				throw new ArgumentException($"Property '{propertyId}' is not supposed to be a float, or is missing from the property type switch.");
+
+			lock (_properties)
+			{
+				if (!_properties.TryGetValue(propertyId, out var property))
+					_properties[propertyId] = new FloatProperty(propertyId, value);
+				else
+					(property as FloatProperty).Value = value;
+			}
+		}
+
+		/// <summary>
+		/// Sets the given propery's value.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <param name="value"></param>
+		public void Set(int propertyId, string value)
+		{
+			if (GetPropertyType(propertyId) != PropertyType.String)
+				throw new ArgumentException($"Property '{propertyId}' is not supposed to be a string, or is missing from the property type switch.");
+
+			lock (_properties)
+			{
+				if (!_properties.TryGetValue(propertyId, out var property))
+					_properties[propertyId] = new StringProperty(propertyId, value);
+				else
+					(property as StringProperty).Value = value;
+			}
+		}
+
+		/// <summary>
+		/// Returns the given property's type.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <returns></returns>
+		public static PropertyType GetPropertyType(int propertyId)
+		{
+			// This is a terrible way to handle this, but it's the only
+			// one we got. Extend this switch case as we find new string
+			// values, which IMC apparently use for anything that's not
+			// fit to be a float, like coordinates or precise, large
+			// numbers.
+
+			switch (propertyId)
+			{
+				case ObjectProperty.Account.Medal_Get_Date:
+				case ObjectProperty.Account.LimitPaymentStateBySteam:
+				case ObjectProperty.PC.JobName:
+				case ObjectProperty.PC.LastExp:
+				case ObjectProperty.PCEtc.Kill_boss_Velmosquy:
+				case ObjectProperty.Party.P_PARTY_Q_030_TicketLifeTime:
+				case ObjectProperty.SessionObject.QSTARTZONETYPE:
+				case ObjectProperty.SessionObject.LastGenPosServer:
+				case ObjectProperty.SessionObject.DROPITEM_REQUEST1_TRL:
+					return PropertyType.String;
+				default:
+					return PropertyType.Float;
+			}
+		}
+	}
+}
---
+++ src/Shared/World/ObjectProperties/Property.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) Aura development team - Licensed under GNU GPL
+// For more information, see licence.txt in the main folder
+
+using System;
+using System.Text;
+
+namespace Melia.Shared.World.ObjectProperties
+{
+	public interface IProperty
+	{
+		/// <summary>
+		/// Returns the property's id.
+		/// </summary>
+		int Id { get; }
+
+		/// <summary>
+		/// Returns the property's size it bytes, as it would take
+		/// up in a packet, incl. the id.
+		/// </summary>
+		int Size { get; }
+
+		/// <summary>
+		/// Returns the property's type.
+		/// </summary>
+		PropertyType Type { get; }
+	}
+
+	/// <summary>
+	/// An object property of type float.
+	/// </summary>
+	public class FloatProperty : IProperty
+	{
+		/// <summary>
+		/// Returns this property's id.
+		/// </summary>
+		public int Id { get; }
+
+		/// <summary>
+		/// Returns the property's type.
+		/// </summary>
+		public PropertyType Type { get { return PropertyType.Float; } }
+
+		/// <summary>
+		/// Returns byte size of float.
+		/// </summary>
+		public int Size { get { return sizeof(int) + sizeof(float); } }
+
+		/// <summary>
+		/// Gets or sets this property's value.
+		/// </summary>
+		public float Value { get; set; }
+
+		/// <summary>
+		/// Creates new instance.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <param name="value"></param>
+		public FloatProperty(int propertyId, float value)
+		{
+			this.Id = propertyId;
+			this.Value = value;
+		}
+	}
+
+	/// <summary>
+	/// An object property of type string.
+	/// </summary>
+	public class StringProperty : IProperty
+	{
+		/// <summary>
+		/// Returns this property's id.
+		/// </summary>
+		public int Id { get; }
+
+		/// <summary>
+		/// Returns the property's type.
+		/// </summary>
+		public PropertyType Type { get { return PropertyType.String; } }
+
+		/// <summary>
+		/// Returns byte size of the string, plus 2 bytes for length and 1
+		/// byte for the null terminator.
+		/// </summary>
+		public int Size { get { return sizeof(int) + sizeof(short) + Encoding.UTF8.GetByteCount(this.Value) + sizeof(byte); } }
+
+		/// <summary>
+		/// Gets or sets this property's value.
+		/// </summary>
+		public string Value { get; set; }
+
+		/// <summary>
+		/// Creates new instance.
+		/// </summary>
+		/// <param name="propertyId"></param>
+		/// <param name="value"></param>
+		public StringProperty(int propertyId, string value)
+		{
+			this.Id = propertyId;
+			this.Value = value ?? throw new ArgumentNullException(nameof(value));
+		}
+	}
+
+	public enum PropertyType
+	{
+		Float,
+		String,
+	}
+}
