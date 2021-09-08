2bc4a5d843058f6088751d7f481eeb22debaff8a
exec <exec@dragonbyte.de>
2021-09-08 02:12:21 +0200

    Commented entity interfaces

--- src/ChannelServer/World/Entity.cs
+++ src/ChannelServer/World/Entity.cs
@@ -6,18 +6,39 @@ using Melia.Shared.World;
 
 namespace Melia.Channel.World
 {
+	/// <summary>
+	/// Describes a living entity that can exist on a map.
+	/// </summary>
 	public interface IEntity
 	{
+		/// <summary>
+		/// Returns the entity's globally unique, session limited, id.
+		/// </summary>
 		int Handle { get; }
+
+		/// <summary>
+		/// Returns the entity's current HP.
+		/// </summary>
 		int Hp { get; }
+
+		/// <summary>
+		/// Returns the map the entity is currently on.
+		/// </summary>
 		Map Map { get; }
+
+		/// <summary>
+		/// Returns the entity's position on its current map.
+		/// </summary>
 		Position Position { get; }
 	}
 
+	/// <summary>
+	/// Describes an entity that can actively participate in combat.
+	/// </summary>
 	public interface IAttackableEntity : IEntity
 	{
 		/// <summary>
-		/// Makes monster take damage and kills it if the HP reach 0.
+		/// Makes entity take damage and kills it if its HP reach 0.
 		/// </summary>
 		/// <param name="damage"></param>
 		/// <param name="from"></param>
@@ -27,15 +48,23 @@ namespace Melia.Channel.World
 	}
 
 	/// <summary>
-	/// Damage Visibility
-	/// Invisible: Sends no damage packet
-	/// Hit: Sends ZC_HIT_INFO
-	/// Skill: Sends ZC_SKILL_HIT_INFO
+	/// Defines how to broadcast an entity taking damage.
 	/// </summary>
 	public enum DamageVisibilityModifier
 	{
+		/// <summary>
+		/// Sends no damage information to clients.
+		/// </summary>
 		Invisible,
+
+		/// <summary>
+		/// Sends ZC_HIT_INFO to broadcast damage.
+		/// </summary>
 		Hit,
+
+		/// <summary>
+		/// Sends ZC_SKILL_HIT_INFO to broadcast damage.
+		/// </summary>
 		Skill,
 	}
 
