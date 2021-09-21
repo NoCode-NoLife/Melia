3aa904789c2aa62f69dd4331712434692f3e7e6d
exec <exec@dragonbyte.de>
2021-09-21 17:44:12 +0200

    Fixed potential crash when using M_DEBUG
    The client does not like null strings.

--- system/scripts/client/_common.lua
+++ system/scripts/client/_common.lua
@@ -1,5 +1,9 @@
 -- Writes string to client's stdout.
 function M_DEBUG(str)
+	if not str then
+		str = ""
+	end
+
 	-- print behaves weirdly, let's use IMC_LOG instead.
 	IMC_LOG("INFO_NORMAL", str)
 end
