6a5992b6723c5e23f9b333587ebfdb527c49142b
exec <exec@dragonbyte.de>
2021-09-22 00:20:30 +0200

    Added doc comments to ScriptState

--- src/ChannelServer/Scripting/ScriptState.cs
+++ src/ChannelServer/Scripting/ScriptState.cs
@@ -4,12 +4,34 @@ using Melia.Shared.Data.Database;
 
 namespace Melia.Channel.Scripting
 {
+	/// <summary>
+	/// State information for a script session.
+	/// </summary>
 	public class ScriptState
 	{
+		/// <summary>
+		/// Returns the connection associated with this state.
+		/// </summary>
 		public ChannelConnection Connection { get; private set; }
+
+		/// <summary>
+		/// Gets or sets pointer to the state's Lua thread.
+		/// </summary>
 		public LuaThread LuaThread { get; set; }
+
+		/// <summary>
+		/// Gets or sets the NPC associated with this state.
+		/// </summary>
 		public Monster CurrentNpc { get; set; }
+
+		/// <summary>
+		/// Gets or sets the current dialog title.
+		/// </summary>
 		public string DialogTitle { get; set; }
+
+		/// <summary>
+		/// Get sor sets the current dialog class name.
+		/// </summary>
 		public string DialogClassName { get; set; }
 
 		/// <summary>
@@ -18,11 +40,19 @@ namespace Melia.Channel.Scripting
 		/// </summary>
 		public ShopData CurrentShop { get; set; }
 
+		/// <summary>
+		/// Creates script state for connection.
+		/// </summary>
+		/// <param name="conn"></param>
 		public ScriptState(ChannelConnection conn)
 		{
 			this.Connection = conn;
 		}
 
+		/// <summary>
+		/// Resets script state, closing the thread and resetting all
+		/// state information.
+		/// </summary>
 		public void Reset()
 		{
 			ChannelServer.Instance.ScriptManager.RemoveThread(this.LuaThread);
