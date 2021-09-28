ec3b16d2d77527cb7805632bcc4fb51232f8318a
Salman T. Khan <salmantkhan@gmail.com>
2021-09-28 16:33:09 -0400

    Fixed Coding Convention
    * using Melia.Channel.World.Entities instead of full namespace qualified reference in parameter Buff.

--- src/ChannelServer/Buffs/Generic/BuffHandler.cs
+++ src/ChannelServer/Buffs/Generic/BuffHandler.cs
@@ -1,5 +1,10 @@
-﻿using Melia.Channel.Buffs.Base;
+﻿using System;
+using Melia.Channel.Buffs.Base;
+using Melia.Channel.Network;
+using Melia.Channel.World;
+using Melia.Channel.World.Entities;
 using Melia.Shared.Const;
+using Melia.Shared.World.ObjectProperties;
 
 namespace Melia.Channel.Buffs.Generic
 {
@@ -8,16 +13,16 @@ namespace Melia.Channel.Buffs.Generic
 	/// </summary>
 	public class BuffHandler : IBuffHandler
 	{
-		public void OnStart(World.Buff buff)
+		public void OnStart(Buff buff)
 		{
 			// Behavior on buff start, one time buff
 		}
-		public void WhileActive(World.Buff buff)
+		public void WhileActive(Buff buff)
 		{
 			// Behavior on buff update time
 		}
 
-		public void OnEnd(World.Buff buff)
+		public void OnEnd(Buff buff)
 		{
 			// Behavior on buff removal
 		}
@@ -29,17 +34,18 @@ namespace Melia.Channel.Buffs.Generic
 	[BuffHandler(BuffId.Heal_Buff)]
 	public class HealBuffHandler : IBuffHandler
 	{
-		public void OnStart(World.Buff buff)
+		public void OnStart(Buff buff)
 		{
 			var target = buff.Target;
-			//target.Heal(HealType.Hp, (target.MaxHp * 10) / 100);
+
+			//target.Heal(HealType.Hp, (int)(target.Properties.GetFloat(PropertyId.PC.MHP) * 5) / 100);
 		}
 
-		public void WhileActive(World.Buff buff)
+		public void WhileActive(Buff buff)
 		{
 		}
 
-		public void OnEnd(World.Buff buff)
+		public void OnEnd(Buff buff)
 		{
 		}
 	}
@@ -50,19 +56,48 @@ namespace Melia.Channel.Buffs.Generic
 	[BuffHandler(BuffId.Common_Slow)]
 	public class CommonSlowHandler : IBuffHandler
 	{
-		public void OnStart(World.Buff buff)
+		public void OnStart(Buff buff)
 		{
 			var target = buff.Target;
 			// TODO Something with Move Speed
 		}
 
-		public void WhileActive(World.Buff buff)
+		public void WhileActive(Buff buff)
 		{
 		}
 
-		public void OnEnd(World.Buff buff)
+		public void OnEnd(Buff buff)
 		{
 			// Reset move speed
+		}
+	}
+
+	/// <summary>
+	/// An oblique fire buff handler implmentation
+	/// </summary>
+	[BuffHandler(BuffId.ObliqueFire_Buff)]
+	public class ObliqueFireBuffHandler : IBuffHandler
+	{
+		public void OnStart(Buff buff)
+		{
+			var target = buff.Target as Character;
+			if (buff.OverbuffCounter <= 3)
+			{
+				target.Properties.Modify(PropertyId.PC.SPEED_BM, 3f);
+				Send.ZC_MOVE_SPEED(target);
+			}
+		}
+
+		public void WhileActive(Buff buff)
+		{
+		}
+
+		public void OnEnd(Buff buff)
+		{
+			var target = buff.Target as Character;
+			// Reset move speed
+			target.Properties.Modify(PropertyId.PC.SPEED_BM, -3f * Math.Min(buff.OverbuffCounter, 3));
+			Send.ZC_MOVE_SPEED(target);
 		}
 	}
 }
