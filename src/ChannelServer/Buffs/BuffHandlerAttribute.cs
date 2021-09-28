e54d129282ad183a8995bf211ce8d845cf0b9c5c
Salman T. Khan <salmantkhan@gmail.com>
2021-09-28 08:51:04 -0400

    Made BuffHandlerAttribute public

--- src/ChannelServer/Buffs/BuffHandlerAttribute.cs
+++ src/ChannelServer/Buffs/BuffHandlerAttribute.cs
@@ -3,25 +3,25 @@ using Melia.Shared.Const;
 
 namespace Melia.Channel.Buffs
 {
-    /// <summary>
-    /// Used to mark buff handler classes, which are then automatically
-    /// loaded by the buff handler manager.
-    /// </summary>
-    class BuffHandlerAttribute : Attribute
-    {
-        /// <summary>
-        /// Returns the ids of the buffs this handler is supposed to
-        /// be used for.
-        /// </summary>
-        public BuffId[] BuffIds { get; }
+	/// <summary>
+	/// Used to mark buff handler classes, which are then automatically
+	/// loaded by the buff handler manager.
+	/// </summary>
+	public class BuffHandlerAttribute : Attribute
+	{
+		/// <summary>
+		/// Returns the ids of the buffs this handler is supposed to
+		/// be used for.
+		/// </summary>
+		public BuffId[] BuffIds { get; }
 
-        /// <summary>
-        /// Creates new attribute for the buff ids.
-        /// </summary>
-        /// <param name="buffIds"></param>
-        public BuffHandlerAttribute(params BuffId[] buffIds)
-        {
-            this.BuffIds = buffIds;
-        }
-    }
+		/// <summary>
+		/// Creates new attribute for the buff ids.
+		/// </summary>
+		/// <param name="buffIds"></param>
+		public BuffHandlerAttribute(params BuffId[] buffIds)
+		{
+			this.BuffIds = buffIds;
+		}
+	}
 }
