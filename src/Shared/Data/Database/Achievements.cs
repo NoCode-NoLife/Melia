ee590b3c0548c49faa0934711d71b56f67d2b905
exec <exec@dragonbyte.de>
2021-10-06 22:31:30 +0200

    Added reading of new achievement data

--- src/Shared/Data/Database/Achievements.cs
+++ src/Shared/Data/Database/Achievements.cs
@@ -8,6 +8,12 @@ namespace Melia.Shared.Data.Database
 	{
 		public int Id { get; set; }
 		public string ClassName { get; set; }
+		public string Category { get; set; }
+		public string Name { get; set; }
+		public string PointName { get; set; }
+		public int PointCount { get; set; }
+		public bool IsRepeatable { get; set; }
+		public bool IsAccountReward { get; set; }
 	}
 
 	/// <summary>
@@ -17,12 +23,18 @@ namespace Melia.Shared.Data.Database
 	{
 		protected override void ReadEntry(JObject entry)
 		{
-			entry.AssertNotMissing("id", "className");
+			entry.AssertNotMissing("id", "className", "category", "name", "point", "count", "repeatable", "account");
 
 			var data = new AchievementData();
 
 			data.Id = entry.ReadInt("id");
 			data.ClassName = entry.ReadString("className");
+			data.Category = entry.ReadString("category");
+			data.Name = entry.ReadString("name");
+			data.PointName = entry.ReadString("point");
+			data.PointCount = entry.ReadInt("count");
+			data.IsRepeatable = entry.ReadBool("repeatable");
+			data.IsAccountReward = entry.ReadBool("account");
 
 			this.Entries[data.Id] = data;
 		}
