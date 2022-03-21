67f37f238e801f5c1892d9648ac8348a2a019d06
exec <exec@dragonbyte.de>
2022-03-21 11:22:33 +0100

    Rearranged sub ops

--- src/Shared/Network/SubOp.cs
+++ src/Shared/Network/SubOp.cs
@@ -27,24 +27,18 @@
 		/// </summary>
 		public static class Zone
 		{
+			public const int Unknown_06 = 0x06;
 			public const int LevelUp = 0x12;
 			public const int ClassLevelUp = 0x15;
+			public const int Skill_16 = 0x16;
 			public const int Unkown_1D = 0x1D;
 			public const int FadeOut = 0x38;
 			public const int BarrackSlotCount = 0x3C;
+			public const int AttackCancel = 0x41;
 			public const int AccountUpdate = 0x4C;
+			public const int Skill_4E = 0x4E;
 			public const int Skill = 0x57;
 			public const int ParticleEffect = 0x61;
-			public const int SetSessionKey = 0x14F;
-			public const int ItemDrop = 0x150;
-			public const int NGSCallback = 0x16E;
-			public const int HatVisibleState = 0x182;
-
-			//
-			public const int Unknown_06 = 0x06;
-			public const int Skill_16 = 0x16;
-			public const int AttackCancel = 0x41;
-			public const int Skill_4E = 0x4E;
 			public const int SetSkillSpeed = 0x77;
 			public const int SetHitDelay = 0x78;
 			public const int Unknown_A1 = 0xA1;
@@ -54,6 +48,10 @@
 			public const int Unknown_EF = 0xF0;
 			public const int Unknown_Map_12A = 0x12B;
 			public const int SetGreetingMessage = 0x136;
+			public const int SetSessionKey = 0x14F;
+			public const int ItemDrop = 0x150;
+			public const int NGSCallback = 0x16E;
+			public const int HatVisibleState = 0x182;
 			public const int UpdateSkillUI = 0x187;
 			public const int AdventureBook = 0x195;
 			public const int Unknown_19B = 0x19C;
