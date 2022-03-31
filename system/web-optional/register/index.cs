2e4d8fb539503ee3490b208ea73d6fd65b28530b
Salman T. Khan <salmantkhan@gmail.com>
2022-03-31 11:41:35 -0400

    Web Registration Password Fix
    * ToS Client requires an MD5 password, so added a hashing function to the script, prior to submitting account creation request to the Database, we create an MD5 hash of the password and then send that to the Database.

--- system/web-optional/register/index.cs
+++ system/web-optional/register/index.cs
@@ -6,6 +6,8 @@
 //---------------------------------------------------------------------------
 
 using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.RegularExpressions;
 using Melia.Shared.Util;
 using Melia.Web;
@@ -57,6 +59,7 @@ public class RegistrationControllerScript : Controller
 				goto L_Send;
 			}
 
+			password1 = this.CreateMD5Hash(password1);
 			WebServer.Instance.Database.CreateAccount(name, password1);
 
 			Log.Info("New account created: {0}", name);
@@ -68,5 +71,26 @@ public class RegistrationControllerScript : Controller
 	L_Send:
 		var engine = server.GetEngine("hbs");
 		response.Send(engine.RenderFile(templatePath, new { error, success, name }));
+	}
+	
+	/// <summary>
+	/// Generate an MD5 hash
+	/// </summary>
+	/// <param name="input"></param>
+	/// <returns></returns>
+	private string CreateMD5Hash(string input)
+	{
+		// Step 1, calculate MD5 hash from input
+		MD5 md5 = MD5.Create();
+		byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+		byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+		// Step 2, convert byte array to hex string
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < hashBytes.Length; i++)
+		{
+			sb.Append(hashBytes[i].ToString("X2"));
+		}
+		return sb.ToString();
 	}
 }
