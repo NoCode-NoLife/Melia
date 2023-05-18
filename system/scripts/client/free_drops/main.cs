649f6b41ac266dbd8f3483639af0d2eecbc8987b
exec <exec@dragonbyte.de>
2023-05-18 15:12:14 +0200

    Readded remaining client scripts

---
+++ system/scripts/client/free_drops/main.cs
@@ -0,0 +1,26 @@
+﻿//--- Melia Script ----------------------------------------------------------
+// Free Drops
+//--- Description -----------------------------------------------------------
+// This client script modifies the item drop/destroy restrictions, so you
+// can drop whatever you want. The server then decides whether the item
+// should actually be destroyable. This way we can allow players to be
+// able to get rid of otherwise undestroyable and unstorable items.
+//---------------------------------------------------------------------------
+
+using Melia.Shared.Scripting;
+using Melia.Zone.Events;
+using Melia.Zone.Scripting;
+
+public class FreeDropsClientScript : ClientScript
+{
+	public override void Load()
+	{
+		this.LoadAllScripts();
+	}
+
+	[On("PlayerReady")]
+	protected void OnPlayerReady(object sender, PlayerEventArgs e)
+	{
+		this.SendAllScripts(e.Character);
+	}
+}
---
+++ system/scripts/client/personal_shops/main.cs
@@ -0,0 +1,25 @@
+﻿//--- Melia Script ----------------------------------------------------------
+// Personal Shops
+//--- Description -----------------------------------------------------------
+// Personal Shops aren't officially supported anymore, and their scripts
+// are partially broken. These client scripts fix them to a certain degree,
+// to make the UI usable again.
+//---------------------------------------------------------------------------
+
+using Melia.Shared.Scripting;
+using Melia.Zone.Events;
+using Melia.Zone.Scripting;
+
+public class PersonalShopsClientScript : ClientScript
+{
+	public override void Load()
+	{
+		this.LoadAllScripts();
+	}
+
+	[On("PlayerReady")]
+	protected void OnPlayerReady(object sender, PlayerEventArgs e)
+	{
+		this.SendAllScripts(e.Character);
+	}
+}
