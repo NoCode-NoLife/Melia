ec98c35bc502adcb7ca2081a4adec643d0d81b7f
exec <exec@dragonbyte.de>
2023-05-24 14:44:10 +0200

    Updated DashRun buff
    Now using the DashRun property to affect the stamina usage instead of setting Sta_Run. Not entirely sure I like this solution, as I'd rather keep everything related to the buff inside its handler, but this is the way the system likes it for the moment. For movement speed we do have a buff property, though I suspect that we're not doing that as intended either. DashRun should presumably just affect the MSPD calculation.

--- src/ZoneServer/Buffs/Handlers/DashRun.cs
+++ src/ZoneServer/Buffs/Handlers/DashRun.cs
@@ -18,10 +18,23 @@ namespace Melia.Zone.Buffs.Handlers
 		public override void OnStart(Buff buff)
 		{
 			var target = (Character)buff.Target;
+			var speedBonus = this.GetSpeedBonus(target);
 
-			// Increase movement speed and add stamina usage over time
-			target.Properties.Modify("MSPD_BM", 10);
-			target.Properties.Modify("Sta_Run", 500);
+			// Usually the game uses DashRun to signal that the actor
+			// is dashing, and this increases the amount of stamina
+			// used during movement in Recovery. This does work, but
+			// it would be nice to have control over the usage amount
+			// from the buff itself. This isn't quite possible as long
+			// as we limit ourselves to properties and the client's
+			// formulas though, as there is no "stamina usage" property.
+			// I'll leave it like this for now, but we might want to
+			// think about adding a "modifier" system that lets us
+			// modify properties temporarily outside of the normal
+			// system.
+
+			// Set dash property and add movement speed bonus
+			target.Properties.SetFloat(PropertyName.DashRun, 1);
+			target.Properties.Modify(PropertyName.MSPD_BM, speedBonus);
 
 			Send.ZC_MOVE_SPEED(target);
 		}
@@ -33,9 +46,10 @@ namespace Melia.Zone.Buffs.Handlers
 		public override void OnEnd(Buff buff)
 		{
 			var target = (Character)buff.Target;
+			var speedBonus = this.GetSpeedBonus(target);
 
-			target.Properties.Modify("MSPD_BM", -10);
-			target.Properties.Modify("Sta_Run", -500);
+			target.Properties.SetFloat(PropertyName.DashRun, 0);
+			target.Properties.Modify(PropertyName.MSPD_BM, -speedBonus);
 
 			Send.ZC_MOVE_SPEED(target);
 		}
@@ -47,7 +61,22 @@ namespace Melia.Zone.Buffs.Handlers
 		/// <param name="buff"></param>
 		public override void WhileActive(Buff buff)
 		{
-			// TODO: Disable if stamina <= 0.
+			var target = (Character)buff.Target;
+
+			var minStamina = target.Properties.GetFloat(PropertyName.Sta_Runable);
+
+			if (target.Properties.Stamina < minStamina)
+				buff.End();
+		}
+
+		/// <summary>
+		/// Returns the speed bonus granted while running.
+		/// </summary>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		private float GetSpeedBonus(Character target)
+		{
+			return 10;
 		}
 	}
 }
