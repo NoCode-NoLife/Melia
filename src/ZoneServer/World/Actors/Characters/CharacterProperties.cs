29fe8470d5cb03ff44dadbec963bcb9acdc31910
exec <exec@dragonbyte.de>
2023-05-29 11:02:08 +0200

    Fixed MATK auto-update

--- src/ZoneServer/World/Actors/Characters/CharacterProperties.cs
+++ src/ZoneServer/World/Actors/Characters/CharacterProperties.cs
@@ -150,6 +150,8 @@ namespace Melia.Zone.World.Actors.Characters
 			this.AutoUpdate("Sta_Recover", new[] { "REST_BM", "RSta_BM" });
 			this.AutoUpdate("MINPATK", new[] { "Lv", "STR", "PATK_BM", "MINPATK_BM", "PATK_MAIN_BM", "MINPATK_MAIN_BM", "PATK_RATE_BM", "MINPATK_RATE_BM", "PATK_MAIN_RATE_BM", "MINPATK_MAIN_RATE_BM" });
 			this.AutoUpdate("MAXPATK", new[] { "Lv", "STR", "PATK_BM", "MAXPATK_BM", "PATK_MAIN_BM", "MAXPATK_MAIN_BM", "PATK_RATE_BM", "MAXPATK_RATE_BM", "PATK_MAIN_RATE_BM", "MAXPATK_MAIN_RATE_BM" });
+			this.AutoUpdate("MINMATK", new[] { "Lv", "INT", "MATK_BM", "MINMATK_BM", "MATK_RATE_BM", "MINMATK_RATE_BM" });
+			this.AutoUpdate("MAXMATK", new[] { "Lv", "INT", "MATK_BM", "MAXMATK_BM", "MATK_RATE_BM", "MAXMATK_RATE_BM" });
 
 			this.AutoUpdateMax("HP", "MHP");
 			this.AutoUpdateMax("SP", "MSP");
