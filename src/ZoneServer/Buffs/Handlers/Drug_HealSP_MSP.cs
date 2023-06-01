ab071e52fe87576344f110ecc4d054e970ba875a
exec <exec@dragonbyte.de>
2023-06-02 00:58:19 +0200

    Added basic SP healing buffs

---
+++ src/ZoneServer/Buffs/Handlers/Drug_HealSP_MSP.cs
@@ -0,0 +1,33 @@
+﻿using Melia.Shared.Tos.Const;
+using Melia.Zone.Buffs.Base;
+
+namespace Melia.Zone.Buffs.Handlers
+{
+	/// <summary>
+	/// Buff handler for SP potions that heal a percentage of the user's
+	/// max SP.
+	/// </summary>
+	[BuffHandler(BuffId.Drug_HealSP_MSP)]
+	public class Drug_HealSP_MSP : BuffHandler
+	{
+		public override void OnStart(Buff buff)
+		{
+			var character = buff.Target;
+
+			var maxSp = character.Properties.GetFloat(PropertyName.MSP);
+			var spHealAmount = maxSp * 0.20f;
+
+			character.Heal(0, spHealAmount);
+		}
+
+		public override void WhileActive(Buff buff)
+		{
+			var character = buff.Target;
+
+			var maxSp = character.Properties.GetFloat(PropertyName.MSP);
+			var spHealAmount = maxSp * 0.20f;
+
+			character.Heal(0, spHealAmount);
+		}
+	}
+}
