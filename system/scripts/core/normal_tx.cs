dddf010c991bb9ce1c91566030844a062697f359
exec <exec@dragonbyte.de>
2023-06-02 18:00:43 +0200

    Added dummy handling for GUIDE_QUEST_OPEN_UI

--- system/scripts/core/normal_tx.cs
+++ system/scripts/core/normal_tx.cs
@@ -1,4 +1,10 @@
-﻿using Melia.Zone.Scripting;
+﻿//--- Melia Script ----------------------------------------------------------
+// Normal Transaction Scripts
+//--- Description -----------------------------------------------------------
+// Handles "Normal TX" requests from the client.
+//---------------------------------------------------------------------------
+
+using Melia.Zone.Scripting;
 using Melia.Zone.World.Actors.Characters;
 using Yggdrasil.Logging;
 
@@ -13,5 +19,28 @@ public class NormalTxScripts : GeneralScript
 			Log.Warning("SCR_TX_PROPERTY_ACTIVE_TOGGLE: User '{0}' tried to toggle ability '{1}', which they either don't have or is passive.", character.Connection.Account.Name, className);
 
 		return NormalTxResult.Okay;
+	}
+
+	[NormalTxScript("GUIDE_QUEST_OPEN_UI")]
+	public NormalTxResult GUIDE_QUEST_OPEN_UI(Character character, string strArg)
+	{
+		switch (strArg)
+		{
+			case "status":
+			{
+				// Sent when opening the character info. Presumably a
+				// request to open the help panel or show a tooltip or
+				// something.
+				return NormalTxResult.Okay;
+			}
+			case "inventory":
+			{
+				// Sent when opening the inventory. Presumably a request
+				// to open the help panel or show a tooltip or something.
+				return NormalTxResult.Okay;
+			}
+		}
+
+		return NormalTxResult.Fail;
 	}
 }
