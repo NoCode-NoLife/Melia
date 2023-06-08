f0f9f87007c0ddc90f363b94821b830d87463e7b
exec <exec@dragonbyte.de>
2023-06-08 09:38:21 +0200

    Minor change
    Added some comments.

--- src/ZoneServer/World/Actors/Monsters/WarpMonster.cs
+++ src/ZoneServer/World/Actors/Monsters/WarpMonster.cs
@@ -2,15 +2,40 @@
 
 namespace Melia.Zone.World.Actors.Monsters
 {
+	/// <summary>
+	/// A monster that represents a warp to another map.
+	/// </summary>
 	public class WarpMonster : MonsterInName
 	{
+		/// <summary>
+		/// The monster class id of a green arrow representing a warp.
+		/// </summary>
 		public const int WarpMonsterClassId = 40001;
 
+		/// <summary>
+		/// Returns the warp name, which is used to display the name
+		/// of the destination.
+		/// </summary>
 		public string WarpName { get; }
 
+		/// <summary>
+		/// Returns the location of the warp.
+		/// </summary>
 		public Location SourceLocation { get; }
+
+		/// <summary>
+		/// Returns the destination of the warp.
+		/// </summary>
 		public Location WarpLocation { get; }
 
+		/// <summary>
+		/// Creates new warp monster.
+		/// </summary>
+		/// <param name="warpName"></param>
+		/// <param name="targetLocationName"></param>
+		/// <param name="sourceLocation"></param>
+		/// <param name="targetLocation"></param>
+		/// <param name="direction"></param>
 		public WarpMonster(string warpName, string targetLocationName, Location sourceLocation, Location targetLocation, Direction direction)
 			: base(WarpMonsterClassId)
 		{
