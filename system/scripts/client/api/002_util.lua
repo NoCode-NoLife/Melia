334e324714fd3856f9861bfd24be054e8a118fb9
exec <exec@dragonbyte.de>
2023-06-09 17:55:51 +0200

    Added first client scripting API functions

---
+++ system/scripts/client/api/002_util.lua
@@ -0,0 +1,18 @@
+Melia.Serialize = function(obj)
+	if obj == nil then
+		return "nil"
+	end
+
+	if type(obj) ~= "table" then
+		return tostring(obj)
+	end
+
+	local s = "{ "
+
+	for k, v in pairs(obj) do
+		if type(k) ~= "number" then k = '"' .. k .. '"' end
+		s = s .. "["..k.."] = " .. Melia.Serialize(v) .. ", "
+	end
+
+	return s .. "}"
+end
