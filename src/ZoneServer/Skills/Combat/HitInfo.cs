6e977ef0ca43f4d52f6454f189e1901732bb2de5
exec <exec@dragonbyte.de>
2023-06-17 01:03:00 +0200

    Added comments to hit info classes

--- src/ZoneServer/Skills/Combat/HitInfo.cs
+++ src/ZoneServer/Skills/Combat/HitInfo.cs
@@ -2,13 +2,38 @@
 
 namespace Melia.Zone.Skills.Combat
 {
+	/// <summary>
+	/// Contains information about a hit and the damage it caused.
+	/// </summary>
 	public class HitInfo
 	{
+		/// <summary>
+		/// Returns the damage dealt.
+		/// </summary>
 		public float Damage { get; }
+
+		/// <summary>
+		/// Returns the target's current HP after the hit.
+		/// </summary>
 		public float Hp { get; }
+
+		/// <summary>
+		/// Returns the HP priority of the current HP value after the hit.
+		/// </summary>
 		public int HpPriority { get; }
+
+		/// <summary>
+		/// Returns the result type of the hit, affecting the hit effect.
+		/// </summary>
 		public HitResultType ResultType { get; }
 
+		/// <summary>
+		/// Creates new hit.
+		/// </summary>
+		/// <param name="damage"></param>
+		/// <param name="hp"></param>
+		/// <param name="hpPriority"></param>
+		/// <param name="resultType"></param>
 		public HitInfo(float damage, float hp, int hpPriority, HitResultType resultType)
 		{
 			this.Damage = damage;
--- src/ZoneServer/Skills/Combat/SkillHitInfo.cs
+++ src/ZoneServer/Skills/Combat/SkillHitInfo.cs
@@ -4,16 +4,55 @@ using Melia.Zone.World.Actors;
 
 namespace Melia.Zone.Skills.Combat
 {
+	/// <summary>
+	/// Contains information about a hit from a skill.
+	/// </summary>
 	public class SkillHitInfo
 	{
+		/// <summary>
+		/// Returns the entity that caused the hit.
+		/// </summary>
 		public ICombatEntity Attacker { get; }
+
+		/// <summary>
+		/// Returns the target of the hit.
+		/// </summary>
 		public ICombatEntity Target { get; }
+
+		/// <summary>
+		/// Returns the skill used in the attack.
+		/// </summary>
 		public Skill Skill { get; }
+
+		/// <summary>
+		/// Returns the hit's damage information.
+		/// </summary>
 		public HitInfo HitInfo { get; }
+
+		/// <summary>
+		/// Returns the delay before the damage is shown.
+		/// </summary>
 		public TimeSpan DamageDelay { get; }
+
+		/// <summary>
+		/// Returns the skill's hit delay, which affects the animations.
+		/// </summary>
 		public TimeSpan SkillHitDelay { get; }
+
+		/// <summary>
+		/// Gets or sets the hit effect displayed on the target.
+		/// </summary>
 		public HitEffect HitEffect { get; set; } = HitEffect.Impact;
 
+		/// <summary>
+		/// Creates new skill hit.
+		/// </summary>
+		/// <param name="attacker"></param>
+		/// <param name="target"></param>
+		/// <param name="skill"></param>
+		/// <param name="damage"></param>
+		/// <param name="damageDelay"></param>
+		/// <param name="skillHitDelay"></param>
 		public SkillHitInfo(ICombatEntity attacker, ICombatEntity target, Skill skill, float damage, TimeSpan damageDelay, TimeSpan skillHitDelay)
 		{
 			this.Attacker = attacker;
