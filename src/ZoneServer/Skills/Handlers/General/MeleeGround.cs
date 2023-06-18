bf4b6bf4c6afbc1962a0cb6ca4832541ef218141
exec <exec@dragonbyte.de>
2023-06-18 16:40:25 +0200

    Cleaned up MeleeGround default handler
    Adds attack delay and support for the common dagger attack skill.

--- src/ZoneServer/Skills/Handlers/General/MeleeGround.cs
+++ src/ZoneServer/Skills/Handlers/General/MeleeGround.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading.Tasks;
+using Melia.Shared.L10N;
 using Melia.Shared.Tos.Const;
 using Melia.Shared.World;
 using Melia.Zone.Network;
@@ -15,36 +16,67 @@ namespace Melia.Zone.Skills.Handlers.General
 	/// <summary>
 	/// Generic handler for melee ground skills.
 	/// </summary>
-	[SkillHandler(SkillId.Normal_Attack)]
+	[SkillHandler(SkillId.Normal_Attack, SkillId.Common_DaggerAries)]
 	public class MeleeGroundSkillHandler : IMeleeGroundSkillHandler
 	{
-		public void Handle(Skill skill, ICombatEntity caster, Position castPosition, Position targetPosition, IList<ICombatEntity> targets)
+		/// <summary>
+		/// Handles usage of the skill.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="originPos"></param>
+		/// <param name="farPos"></param>
+		/// <param name="targets"></param>
+		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, IList<ICombatEntity> targets)
 		{
-			if (skill.SpendSp > 0)
-				caster.Properties.Modify(PropertyName.SP, -skill.SpendSp);
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
 
 			skill.IncreaseOverheat();
 			caster.Components.Get<CombatComponent>().SetAttackState(true);
 
-			var hits = new List<SkillHitInfo>();
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos, null);
 
-			var damage = SCR_GetRandomAtk(caster, null, skill);
+			this.Attack(skill, caster, originPos, farPos, targets);
+		}
+
+		/// <summary>
+		/// Executes the actual attack after a potential delay.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="castPosition"></param>
+		/// <param name="targetPosition"></param>
+		/// <param name="targets"></param>
+		private async void Attack(Skill skill, ICombatEntity caster, Position castPosition, Position targetPosition, IEnumerable<ICombatEntity> targets)
+		{
+			// Based on Normal_Attack posessing a hit delay of 100ms,
+			// and Common_DaggerAries one of 50ms, and these two values
+			// matching the logs almost 1:1, we can assume this to be
+			// the correct value for the skill hit delay. Not a clue
+			// about damage delay though. Though there are potentially
+			// related values in older skill_bytool files.
+			var damageDelay = TimeSpan.FromMilliseconds(skill.Id == SkillId.Normal_Attack ? 330 : 250);
+			var skillHitDelay = skill.Properties.HitDelay;
+
+			if (skillHitDelay > TimeSpan.Zero)
+				await Task.Delay(skillHitDelay);
+
+			var hits = new List<SkillHitInfo>();
 
 			foreach (var target in targets)
 			{
-				if (!caster.CanAttack(target))
-					continue;
-
+				var damage = SCR_CalculateDamage(caster, target, skill);
 				target.TakeDamage(damage, caster);
 
-				var skillHitInfo = new SkillHitInfo(caster, target, skill, damage, TimeSpan.FromMilliseconds(306), TimeSpan.FromMilliseconds(50));
-				hits.Add(skillHitInfo);
+				var hit = new SkillHitInfo(caster, target, skill, damage, damageDelay, skillHitDelay);
+				hits.Add(hit);
 			}
 
-			Send.ZC_SKILL_MELEE_GROUND(caster, skill, targetPosition, null);
-
-			if (hits.Any())
-				Send.ZC_SKILL_HIT_INFO(caster, hits);
+			Send.ZC_SKILL_HIT_INFO(caster, hits);
 		}
 	}
 }
