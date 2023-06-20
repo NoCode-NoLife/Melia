30e388657481e9522b902330937992e6a4617e1f
exec <exec@dragonbyte.de>
2023-06-20 09:57:39 +0200

    Added Smite buff handler

---
+++ src/ZoneServer/Buffs/Handlers/Smite_Buff.cs
@@ -0,0 +1,24 @@
+﻿using Melia.Shared.Tos.Const;
+using Melia.Zone.Buffs.Base;
+
+namespace Melia.Zone.Buffs.Handlers
+{
+	/// <summary>
+	/// Handle for the Smite Buff, which increases the target's attack.
+	/// </summary>
+	[BuffHandler(BuffId.Smite_Buff)]
+	public class Smite_Buff : BuffHandler
+	{
+		private const float AtkRateBonus = 0.1f; // 10%
+
+		public override void OnStart(Buff buff)
+		{
+			buff.Target.Properties.Modify(PropertyName.PATK_MAIN_RATE_BM, AtkRateBonus);
+		}
+
+		public override void OnEnd(Buff buff)
+		{
+			buff.Target.Properties.Modify(PropertyName.PATK_MAIN_RATE_BM, -AtkRateBonus);
+		}
+	}
+}
