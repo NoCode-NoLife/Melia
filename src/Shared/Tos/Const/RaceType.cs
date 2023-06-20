9a19c83f4de112b2b4b24ccf2cd54ce65d533b05
exec <exec@dragonbyte.de>
2023-06-20 11:35:56 +0200

    Fixed a couple monster race typos

--- src/Shared/Tos/Const/RaceType.cs
+++ src/Shared/Tos/Const/RaceType.cs
@@ -2,13 +2,44 @@
 {
 	public enum RaceType
 	{
+		/// <summary>
+		/// No special race.
+		/// </summary>
 		None,
+
+		/// <summary>
+		/// Mutant
+		/// </summary>
 		Paramune,
+
+		/// <summary>
+		/// Beast
+		/// </summary>
 		Widling,
+
+		/// <summary>
+		/// Demon
+		/// </summary>
 		Velnias,
+
+		/// <summary>
+		/// Item
+		/// </summary>
 		Item,
+
+		/// <summary>
+		/// Plant
+		/// </summary>
 		Forester,
+
+		/// <summary>
+		/// Insect
+		/// </summary>
 		Klaida,
-		Velnais,
+
+		// The client has some demons as "Velnais" instead of "Velnias".
+		// For Melia we corrected these typos and hence disabled this
+		// value.
+		//Velnais,
 	}
 }
