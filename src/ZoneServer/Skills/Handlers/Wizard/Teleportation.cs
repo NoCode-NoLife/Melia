a489cdd570801790519ab4cd9d152644bc38c6c8
exec <exec@dragonbyte.de>
2023-06-25 02:09:02 +0200

    Added untested Psychokino Teleportation handler

--- src/ZoneServer/Skills/Handlers/Wizard/Teleportation.cs
+++ src/ZoneServer/Skills/Handlers/Wizard/Teleportation.cs
@@ -55,4 +55,70 @@ namespace Melia.Zone.Skills.Handlers.Wizard
 			Send.ZC_SET_POS(caster, targetPos);
 		}
 	}
+
+	/// <summary>
+	/// Handler for the Psychokino skill Teleportation.
+	/// </summary>
+	/// <remarks>
+	/// Experimental and untested handler for the Psychokino version
+	/// of the Teleportation skill that is no longer in use. The main
+	/// difference to the Wizard Teleportation is that this one allows
+	/// the caster to teleport back to their previous location if they
+	/// use the skill again within a certain time frame.
+	/// </remarks>
+	[SkillHandler(SkillId.Psychokino_Teleportation)]
+	public class PsychokinoTeleportation : IGroundSkillHandler
+	{
+		private const float TeleportationDistance = 100;
+		private static readonly TimeSpan ReUseTime = TimeSpan.FromSeconds(2);
+
+		/// <summary>
+		/// Handles skill, teleporting caster.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="originPos"></param>
+		/// <param name="farPos"></param>
+		/// <param name="target"></param>
+		/// <exception cref="NotImplementedException"></exception>
+		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, ICombatEntity target)
+		{
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
+
+			skill.IncreaseOverheat();
+			caster.Components.Get<CombatComponent>().SetAttackState(true);
+
+			caster.Components.Get<BuffComponent>().Start(BuffId.Teleportation_Buff, 0, 0, TimeSpan.FromSeconds(1), caster);
+			caster.Components.Get<BuffComponent>().Start(BuffId.Skill_NoDamage_Buff, 0, 0, TimeSpan.FromSeconds(1), caster);
+
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos, null);
+
+			var lastUse = skill.Vars.Get<DateTime>("Melia.LastUse", DateTime.Now);
+			var usedRecently = (DateTime.Now - lastUse) < ReUseTime;
+			var isSecondUse = usedRecently && skill.OverheatCounter >= 1;
+
+			Position targetPos;
+			if (isSecondUse && skill.Vars.TryGet<Position>("Melia.LastPos", out var lastPos))
+			{
+				targetPos = lastPos;
+			}
+			else
+			{
+				targetPos = caster.Position.GetRelative(caster.Direction, TeleportationDistance);
+				if (!caster.Map.Ground.TryGetHeightAt(targetPos, out var height))
+					return;
+
+				targetPos.Y = height;
+			}
+
+			skill.Vars.Set("Melia.LastPos", caster.Position);
+
+			caster.Position = targetPos;
+			Send.ZC_SET_POS(caster, targetPos);
+		}
+	}
 }
