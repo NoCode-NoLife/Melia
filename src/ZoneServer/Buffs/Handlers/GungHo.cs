1f5190f382d741db4b3ebd63d2af397c7234398d
exec <exec@dragonbyte.de>
2023-06-27 13:05:55 +0200

    Added Gung Ho skill and buff handlers

---
+++ src/ZoneServer/Buffs/Handlers/GungHo.cs
@@ -0,0 +1,35 @@
+﻿using Melia.Shared.Tos.Const;
+using Melia.Zone.Buffs.Base;
+
+namespace Melia.Zone.Buffs.Handlers
+{
+	/// <summary>
+	/// Handle for the Smite Buff, which increases the target's attack.
+	/// </summary>
+	[BuffHandler(BuffId.GungHo)]
+	public class GungHo : BuffHandler
+	{
+		private const string VarName = "Melia.AtkRateBonus";
+		private const float BonusPerLevel = 0.02f; // 2%
+
+		public override void OnStart(Buff buff)
+		{
+			var bonus = this.GetAtkRateBonus(buff);
+			buff.Vars.SetFloat(VarName, bonus);
+
+			buff.Target.Properties.Modify(PropertyName.PATK_RATE_BM, bonus);
+		}
+
+		public override void OnEnd(Buff buff)
+		{
+			if (buff.Vars.TryGetFloat(VarName, out var bonus))
+				buff.Target.Properties.Modify(PropertyName.PATK_RATE_BM, -bonus);
+		}
+
+		private float GetAtkRateBonus(Buff buff)
+		{
+			var skillLevel = buff.NumArg1;
+			return skillLevel * BonusPerLevel;
+		}
+	}
+}
