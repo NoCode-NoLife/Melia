96cf9d65951dc9a98acbdd2bfb6de335046fa43c
Felipe Castagnaro de Carvalho <felipecastagnarodecarvalho@gmail.com>
2023-06-27 20:33:09 -0300

    Adding ObliqueFire and DaggerSlash Skills (Scout)

---
+++ src/ZoneServer/Skills/Handlers/Scout/DaggerSlash.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Melia.Shared.L10N;
+using Melia.Shared.Tos.Const;
+using Melia.Shared.World;
+using Melia.Zone.Network;
+using Melia.Zone.Skills.Combat;
+using Melia.Zone.Skills.Handlers.Base;
+using Melia.Zone.Skills.SplashAreas;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.CombatEntities.Components;
+using static Melia.Zone.Skills.SkillUseFunctions;
+using System.Threading.Tasks;
+
+namespace Melia.Zone.Skills.Handlers.Scout
+{
+	/// <summary>
+	/// Handler for the Scout skill Dagger Slash.
+	/// </summary>
+	[SkillHandler(SkillId.Scout_DaggerSlash)]
+	public class DaggerSlash : IGroundSkillHandler
+	{
+		/// <summary>
+		/// Handles skill, do a slash attack to the nearby enemies.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="originPos"></param>
+		/// <param name="farPos"></param>
+		/// <param name="designatedTarget"></param>
+		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, ICombatEntity designatedTarget)
+		{
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
+
+			skill.IncreaseOverheat();
+			caster.TurnTowards(designatedTarget);
+			caster.Components.Get<CombatComponent>().SetAttackState(true);
+
+			// Get splash area
+			var splashAreaHeight = 40;
+			var splashAreaWidth = 25;
+
+			// We'll ignore the data sent by the client and get the positions ourselves
+			originPos = caster.Position;
+			farPos = originPos.GetRelative(caster.Direction, splashAreaHeight);
+
+			var splashArea = new Square(originPos, caster.Direction, splashAreaHeight, splashAreaWidth);
+
+			Debug.ShowShape(caster.Map, splashArea, edgePoints: false);
+
+			Send.ZC_SKILL_READY(caster, skill, originPos, farPos);
+
+			this.Attack(skill, caster, splashArea, farPos);
+		}
+
+		/// <summary>
+		/// Executes the actual attack after a delay.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="splashArea"></param>
+		private async void Attack(Skill skill, ICombatEntity caster, ISplashArea splashArea, Position farPos)
+		{
+			var hits = new List<SkillHitInfo>();
+			
+			//HardCoded Values - @TODO: Make it dynamic
+			var SkillFactor = 0.61f;
+			var AoeAttackRatio = 6;
+
+			var hitTime = TimeSpan.FromMilliseconds(300);
+			var secondHitTime = TimeSpan.FromMilliseconds(550);
+			var damageDelay = TimeSpan.FromMilliseconds(270);
+			var damageSecondDelay = TimeSpan.FromMilliseconds(550);
+			var skillHitDelay = TimeSpan.Zero;
+			
+			var targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
+
+			if (targets.Any() && targets.Count > 6)
+			{
+				targets = targets.GetRange(0, AoeAttackRatio);
+			}
+
+			await Task.Delay(hitTime);
+
+			foreach (var target in targets)
+			{
+				var skillHitResult = SCR_SkillHit(caster, target, skill);
+				var damage = skillHitResult.Damage * SkillFactor / 2;
+
+				target.TakeDamage(damage, caster);
+
+				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, damageDelay, skillHitDelay);
+				skillHit.ForceId = ForceId.GetNew();
+
+				hits.Add(skillHit);
+			}
+
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos, hits);
+
+			await Task.Delay(secondHitTime);
+
+			hits = new List<SkillHitInfo>();
+
+			foreach (var target in targets)
+			{
+				var skillHitResult = SCR_SkillHit(caster, target, skill);
+				var damage = skillHitResult.Damage * SkillFactor / 2;
+
+				target.TakeDamage(damage, caster);
+
+				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, damageSecondDelay, skillHitDelay);
+				skillHit.ForceId = ForceId.GetNew();
+
+				hits.Add(skillHit);
+			}
+
+			Send.ZC_SKILL_HIT_INFO(caster, hits);						
+		}
+	}
+}
---
+++ src/ZoneServer/Skills/Handlers/Scout/ObliqueFire.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using Melia.Shared.L10N;
+using Melia.Shared.Tos.Const;
+using Melia.Shared.World;
+using Melia.Zone.Network;
+using Melia.Zone.Skills.Combat;
+using Melia.Zone.Skills.Handlers.Base;
+using Melia.Zone.Skills.SplashAreas;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.CombatEntities.Components;
+using Yggdrasil.Extensions;
+using static Melia.Zone.Skills.SkillUseFunctions;
+
+namespace Melia.Zone.Skills.Handlers.Scout
+{
+	/// <summary>
+	/// Handles the Scout skill Oblique Fire.
+	/// </summary>
+	[SkillHandler(SkillId.Scout_ObliqueFire)]
+	public class ObliqueFire : ITargetSkillHandler
+	{
+		/// <summary>
+		/// Handles the skill, shoot missile at enemy that spreads to another target.
+		/// </summary>
+		public void Handle(Skill skill, ICombatEntity caster, ICombatEntity designatedTarget)
+		{
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
+
+			skill.IncreaseOverheat();
+			caster.TurnTowards(designatedTarget);
+			caster.Components.Get<CombatComponent>().SetAttackState(true);
+
+			if (designatedTarget == null)
+			{
+				Send.ZC_SKILL_FORCE_TARGET(caster, null, skill, null);
+				return;
+			}
+
+			if (!caster.Position.InRange2D(designatedTarget.Position, skill.Data.MaxRange))
+			{
+				caster.ServerMessage(Localization.Get("Too far away."));
+				return;
+			}
+
+			var damageDelay = TimeSpan.FromMilliseconds(200);
+			var skillHitDelay = skill.Properties.HitDelay;
+
+			var skillHits = new List<SkillHitInfo>();
+
+			var skillHitResult = SCR_SkillHit(caster, designatedTarget, skill);
+			designatedTarget.TakeDamage(skillHitResult.Damage, caster);
+
+			var skillHit = new SkillHitInfo(caster, designatedTarget, skill, skillHitResult, damageDelay, skillHitDelay);
+			skillHit.ForceId = ForceId.GetNew();
+
+			skillHits.Add(skillHit);
+
+			var originPos = caster.Position;
+			var hits = new List<HitInfo>();
+			var splashArea = new Circle(designatedTarget.Position, skill.Data.SplashHeight * 2);
+			var targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
+
+			if (targets.Any())
+			{
+				var targetDistanceDic = new Dictionary<Position, ICombatEntity>();
+				var distancesList = new List<Position>();
+
+				foreach (var target in targets)
+				{
+					if (target != designatedTarget)
+					{
+						if (!targetDistanceDic.ContainsKey(target.Position))
+						{
+							targetDistanceDic.Add(target.Position, target);
+						}
+						
+						distancesList.Add(target.Position);
+					}
+				}
+
+				var nearstTargetDistance = distancesList.OrderByDescending(v2 => GetDistance(designatedTarget.Position, v2)).LastOrDefault();
+
+				var nearstTarget = targetDistanceDic[nearstTargetDistance];
+
+				if (nearstTarget != null)
+				{
+					var skillHitResultPost = SCR_SkillHit(caster, nearstTarget, skill);
+					nearstTarget.TakeDamage(skillHitResultPost.Damage, caster);
+
+					var skillHitPost = new SkillHitInfo(caster, nearstTarget, skill, skillHitResultPost, damageDelay, skillHitDelay);
+					skillHitPost.ForceId = ForceId.GetNew();
+
+					skillHits.Add(skillHitPost);
+				}
+			}
+
+			Send.ZC_SKILL_FORCE_TARGET(caster, designatedTarget, skill, skillHits);
+		}
+
+		private static double GetDistance(Position initialPosition, Position mobPosition)
+		{
+			return Math.Sqrt(Math.Pow((mobPosition.X - initialPosition.X), 2) + Math.Pow((mobPosition.Y - initialPosition.Y), 2));
+		}
+	}
+}
