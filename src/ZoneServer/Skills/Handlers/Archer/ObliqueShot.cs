2429a4894d8fb4493b23fb289173772a38e61436
exec <exec@dragonbyte.de>
2023-07-05 14:04:58 +0200

    Cleaned up Oblique Shot handler

--- src/ZoneServer/Skills/Handlers/Archer/ObliqueShot.cs
+++ src/ZoneServer/Skills/Handlers/Archer/ObliqueShot.cs
@@ -23,7 +23,7 @@ namespace Melia.Zone.Skills.Handlers.Archer
 		/// <summary>
 		/// Handles the skill, shoot missile at enemy that spreads to another target.
 		/// </summary>
-		public void Handle(Skill skill, ICombatEntity caster, ICombatEntity designatedTarget)
+		public void Handle(Skill skill, ICombatEntity caster, ICombatEntity target)
 		{
 			if (!caster.TrySpendSp(skill))
 			{
@@ -32,69 +32,75 @@ namespace Melia.Zone.Skills.Handlers.Archer
 			}
 
 			skill.IncreaseOverheat();
-			caster.TurnTowards(designatedTarget);
+			caster.TurnTowards(target);
+			caster.Components.Get<CombatComponent>().SetAttackState(true);
 
-			if (designatedTarget == null)
+			if (target == null)
 			{
+				Send.ZC_SKILL_FORCE_TARGET(caster, null, skill, null);
 				return;
 			}
 
-			if (!caster.Position.InRange2D(designatedTarget.Position, skill.Data.MaxRange))
+			if (!caster.Position.InRange2D(target.Position, skill.Data.MaxRange))
 			{
 				caster.ServerMessage(Localization.Get("Too far away."));
+				Send.ZC_SKILL_FORCE_TARGET(caster, null, skill, null);
 				return;
 			}
 
-			this.Attack(skill, caster, designatedTarget);
+			var damageDelay = TimeSpan.FromMilliseconds(45);
+			var skillHitDelay = TimeSpan.Zero;
+
+			var skillHitResult = SCR_SkillHit(caster, target, skill);
+			target.TakeDamage(skillHitResult.Damage, caster);
+
+			var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, damageDelay, skillHitDelay);
+			skillHit.ForceId = ForceId.GetNew();
+
+			Send.ZC_SKILL_FORCE_TARGET(caster, target, skill, skillHit);
+
+			// Random chance to apply Slow debuff	
+			if (RandomProvider.Next(100) < 50)
+			{
+				var duration = TimeSpan.FromSeconds(7);
+				target.Components.Get<BuffComponent>().Start(BuffId.Common_Slow, skill.Level, 0, duration, caster);
+			}
+
+			// Bounce shot
+			if (this.TryGetBounceTarget(caster, target, skill, out var bounceTarget))
+			{
+				skillHitResult = SCR_SkillHit(caster, bounceTarget, skill);
+				bounceTarget.TakeDamage(skillHitResult.Damage, caster);
+
+				var hit = new HitInfo(caster, target, skill, skillHitResult);
+				Send.ZC_HIT_INFO(caster, bounceTarget, skill, hit);
+			}
 		}
 
 		/// <summary>
-		/// Executes the actual attack.
+		/// Returns the closest target to the main target to bounce the
+		/// attack off to.
 		/// </summary>
-		/// <param name="skill"></param>
 		/// <param name="caster"></param>
-		/// <param name="designatedTarget"></param>
-		private async void Attack(Skill skill, ICombatEntity caster, ICombatEntity designatedTarget)
+		/// <param name="mainTarget"></param>
+		/// <param name="skill"></param>
+		/// <param name="bounceTarget"></param>
+		/// <returns></returns>
+		private bool TryGetBounceTarget(ICombatEntity caster, ICombatEntity mainTarget, Skill skill, out ICombatEntity bounceTarget)
 		{
-			var damageDelay = TimeSpan.Zero;
-			var skillHitDelay = TimeSpan.Zero;
-			var secondHitTime = TimeSpan.FromMilliseconds(100);
+			var splashPos = caster.Position;
+			var splashRadius = 50; // SplashHeight * 2?
+			var splashArea = new Circle(mainTarget.Position, splashRadius);
 
-			var skillHitResult = SCR_SkillHit(caster, designatedTarget, skill);
-			designatedTarget.TakeDamage(skillHitResult.Damage, caster);
-
-			var skillHit = new SkillHitInfo(caster, designatedTarget, skill, skillHitResult, damageDelay, skillHitDelay);
-			skillHit.ForceId = ForceId.GetNew();
-
-			Send.ZC_SKILL_FORCE_TARGET(caster, designatedTarget, skill, skillHit);
-
-			var random = RandomProvider.Get();
-
-			// It has 50% of being giving slow of 7 seconds to the target enemy		
-			if (random.Next(2) == 0)
-			{
-				// TODO: make the enemy show the slow emoticon
-				designatedTarget.Components.Get<BuffComponent>().Start(BuffId.Common_Slow, skill.Level, 0, TimeSpan.FromSeconds(7), designatedTarget);
-			}
-
-			await Task.Delay(secondHitTime);
-
-			var originPos = caster.Position;
-			var splashArea = new Circle(designatedTarget.Position, skill.Data.SplashHeight * 2);
 			var targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
-
-			var nearestTarget = targets.Where(a => a != designatedTarget).OrderBy(a => a.Position.Get2DDistance(designatedTarget.Position)).FirstOrDefault();
-
-			if (nearestTarget != null)
+			if (!targets.Any())
 			{
-				var skillHitResultPost = SCR_SkillHit(caster, nearestTarget, skill);
-				nearestTarget.TakeDamage(skillHitResultPost.Damage, caster);
-
-				var skillHitPost = new SkillHitInfo(caster, nearestTarget, skill, skillHitResultPost, damageDelay, skillHitDelay);
-				var hit = new HitInfo(caster, nearestTarget, skill, skillHitResult.Damage / 2, skillHitResult.Result);
-
-				Send.ZC_HIT_INFO(caster, nearestTarget, skill, hit);
+				bounceTarget = null;
+				return false;
 			}
+
+			bounceTarget = targets.Where(a => a != mainTarget).OrderBy(a => a.Position.Get2DDistance(mainTarget.Position)).FirstOrDefault();
+			return bounceTarget != null;
 		}
 	}
 }
