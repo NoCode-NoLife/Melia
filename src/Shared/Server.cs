04ec4fa5aaf236a3e91baf11994b6a6af7b7c6c0
exec <exec@dragonbyte.de>
2023-07-14 15:45:02 +0200

    Added recipe data

--- src/Shared/Server.cs
+++ src/Shared/Server.cs
@@ -236,6 +236,7 @@ namespace Melia.Shared
 					this.LoadDb(this.Data.NormalTxDb, "db/normal_tx_scripts.txt");
 					this.LoadDb(this.Data.PacketStringDb, "db/packetstrings.txt");
 					this.LoadDb(this.Data.PropertiesDb, "db/properties.txt");
+					this.LoadDb(this.Data.RecipeDb, "db/recipes.txt");
 					this.LoadDb(this.Data.ResurrectionPointDb, "db/resurrection_points.txt");
 					this.LoadDb(this.Data.ServerDb, "db/servers.txt");
 					this.LoadDb(this.Data.SessionObjectDb, "db/sessionobjects.txt");
