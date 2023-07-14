a5a4c5a2695fdeabadc4bd4e338a7cc72ac48a93
exec <exec@dragonbyte.de>
2023-07-14 10:16:12 +0200

    Fixed amounts in monsterinfo command

--- src/ZoneServer/Commands/ChatCommands.Handlers.cs
+++ src/ZoneServer/Commands/ChatCommands.Handlers.cs
@@ -878,9 +878,9 @@ namespace Melia.Zone.Commands
 							if (displayAmount)
 							{
 								if (minAmount == maxAmount)
-									monsterEntry.AppendFormat("{{nl}}- {0} {1} ({2:0.####}%)", currentDrop.MinAmount, itemData.Name, dropChance);
+									monsterEntry.AppendFormat("{{nl}}- {0} {1} ({2:0.####}%)", minAmount, itemData.Name, dropChance);
 								else
-									monsterEntry.AppendFormat("{{nl}}- {0}~{1} {2} ({3:0.####}%){{nl}}", currentDrop.MinAmount, currentDrop.MaxAmount, itemData.Name, dropChance);
+									monsterEntry.AppendFormat("{{nl}}- {0}~{1} {2} ({3:0.####}%){{nl}}", minAmount, maxAmount, itemData.Name, dropChance);
 							}
 							else
 							{
