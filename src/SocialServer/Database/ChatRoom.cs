cbd7b1bc5f8d17c56eb1b46fcc82d39e58183488
exec <exec@dragonbyte.de>
2023-07-20 23:28:03 +0200

    Minor change
    Moved chat message class.

--- src/SocialServer/Database/ChatRoom.cs
+++ src/SocialServer/Database/ChatRoom.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Melia.Shared.Network;
 
 namespace Melia.Social.Database
@@ -123,55 +122,6 @@ namespace Melia.Social.Database
 						user.Connection.Send(packet);
 				}
 			}
-		}
-	}
-
-	/// <summary>
-	/// Represents a message in a chat room
-	/// </summary>
-	public class ChatMessage
-	{
-		/// <summary>
-		/// Returns the account of the chat message's sender.
-		/// </summary>
-		public Account Sender { get; }
-
-		/// <summary>
-		/// Returns the account of the chat message's recipient.
-		/// </summary>
-		public Account Recipient { get; }
-
-		/// <summary>
-		/// Returns the message that was sent.
-		/// </summary>
-		public string Message { get; }
-
-		/// <summary>
-		/// Returns the time the message was sent.
-		/// </summary>
-		public DateTime SentTime { get; } = DateTime.Now;
-
-		/// <summary>
-		/// Creates new chat message.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="message"></param>
-		public ChatMessage(Account sender, string message)
-			: this(sender, null, message)
-		{
-		}
-
-		/// <summary>
-		/// Creates new chat message.
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="recipient"></param>
-		/// <param name="message"></param>
-		public ChatMessage(Account sender, Account recipient, string message)
-		{
-			this.Sender = sender;
-			this.Recipient = recipient;
-			this.Message = message;
 		}
 	}
 
