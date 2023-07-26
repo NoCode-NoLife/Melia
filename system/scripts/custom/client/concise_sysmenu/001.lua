123f8202e6a86571dfb233fb56496534ab1f8c2b
exec <exec@dragonbyte.de>
2023-07-26 23:23:31 +0200

    Added simplified party button to concise sysmenu

--- system/scripts/custom/client/concise_sysmenu/001.lua
+++ system/scripts/custom/client/concise_sysmenu/001.lua
@@ -2,10 +2,12 @@
 
 Melia.Ui.SysMenu.RemoveButton("BtnCollection")
 Melia.Ui.SysMenu.RemoveButton("BtnInstantDungeon")
+Melia.Ui.SysMenu.RemoveButton("BtnParty")
 Melia.Ui.SysMenu.RemoveButton("BtnAdvancement")
 Melia.Ui.SysMenu.RemoveButton("BtnFishing")
 Melia.Ui.SysMenu.RemoveButton("BtnGuildPromo")
 Melia.Ui.SysMenu.RemoveButton("BtnPcBang")
 Melia.Ui.SysMenu.InsertButton(2, "BtnAdvancement", "sysmenu_jobinfo", DicID("UI_20180208_003013"), "OPEN_RANKROLLBACK_UI_BY_SYSMENU()")
+Melia.Ui.SysMenu.InsertButton(6, "BtnParty", "sysmenu_party", DicID("UI_20220204_005169"), "ui.ToggleFrame('party')")
 
 Melia.Ui.SysMenu.ResumeRefresh()
