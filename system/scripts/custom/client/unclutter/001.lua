4ae5a198e8ecc7fb526c88f3fbd5e5deda11f8dd
exec <exec@dragonbyte.de>
2023-12-27 21:58:18 +0100

    Updated unclutter custom client script
    Adds the removal of the popo boost button. Whatever that is...

--- system/scripts/custom/client/unclutter/001.lua
+++ system/scripts/custom/client/unclutter/001.lua
@@ -9,5 +9,7 @@ M_REMOVE_ELEMENT("Open Boards", "minimized_total_board_button", "openTotalBoardB
 M_REMOVE_ELEMENT("Open Party", "minimized_total_party_button", "openTotalPartyBtn")
 M_REMOVE_ELEMENT("Grow Up", "minimized_guidequest_button", "openBtn")
 M_REMOVE_ELEMENT("TP", "openingameshopbtn", "open_openingameshopbtn")
+M_REMOVE_ELEMENT("Popo Boost", "minimized_popoboost_2023", "openPoPoBoost")
+M_REMOVE_ELEMENT("Popo Boost Notice", "minimized_popoboost_2023", "notice_bg")
 M_REMOVE_ELEMENT("Fullscreen Menu", "minimized_fullscreen_navigation_menu_button", "btn_open")
 M_REMOVE_ELEMENT("Fullscreen Menu", "minimized_fullscreen_navigation_menu_button", "text_name")
