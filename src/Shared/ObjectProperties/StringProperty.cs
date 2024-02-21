9968b63c75fbfaff515fcefe39319e53d67d121c
exec <exec@dragonbyte.de>
2024-02-21 23:53:12 +0100

    Added reference string property

--- src/Shared/ObjectProperties/StringProperty.cs
+++ src/Shared/ObjectProperties/StringProperty.cs
@@ -1,4 +1,6 @@
-﻿namespace Melia.Shared.ObjectProperties
+﻿using System;
+
+namespace Melia.Shared.ObjectProperties
 {
 	/// <summary>
 	/// A string-type property.
@@ -13,6 +15,33 @@
 		public StringProperty(string ident, string value = null)
 			: base(ident, value)
 		{
+		}
+	}
+
+	/// <summary>
+	/// A string-type property that returns a referenced value.
+	/// </summary>
+	public class RStringProperty : StringProperty, IUnsettableProperty, IProperty
+	{
+		private readonly Func<string> _getter;
+
+		/// <summary>
+		/// Returns the calculated value of the property.
+		/// </summary>
+		public override string Value
+		{
+			get => _getter();
+			set => throw new InvalidOperationException($"Referenced property '{this.Ident}' in '{this.GetType().Name}' cannot be set.");
+		}
+
+		/// <summary>
+		/// Creates new property.
+		/// </summary>
+		/// <param name="ident"></param>
+		/// <param name="getter"></param>
+		public RStringProperty(string ident, Func<string> getter) : base(ident, null)
+		{
+			_getter = getter;
 		}
 	}
 }
