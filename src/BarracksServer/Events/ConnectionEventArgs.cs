5b4d7849c23f53be95bda4225a478c6b2103c4ab
exec <exec@dragonbyte.de>
2024-03-04 01:03:22 +0100

    Added basic barracks scripting support
    Adds loading scripts for the barracks server and a couple basic events to react to. Among other things, this gives scripters the ability to change a character's starting map.

---
+++ src/BarracksServer/Events/ConnectionEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Melia.Barracks.Network;
+
+namespace Melia.Barracks.Events
+{
+	/// <summary>
+	/// Event arguments related to a connection.
+	/// </summary>
+	public class ConnectionEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Returns the related connection.
+		/// </summary>
+		public IBarracksConnection Connection { get; }
+
+		/// <summary>
+		/// Creates new instance.
+		/// </summary>
+		/// <param name="connection"></param>
+		public ConnectionEventArgs(IBarracksConnection connection)
+		{
+			this.Connection = connection;
+		}
+	}
+}
--- src/BarracksServer/Network/PacketHandler.cs
+++ src/BarracksServer/Network/PacketHandler.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using Melia.Barracks.Database;
+using Melia.Barracks.Events;
 using Melia.Shared.Database;
 using Melia.Shared.L10N;
 using Melia.Shared.Network;
@@ -93,6 +94,8 @@ namespace Melia.Barracks.Network
 			Log.Info("User '{0}' logged in.", conn.Account.Name);
 
 			Send.BC_LOGINOK(conn);
+
+			BarracksServer.Instance.ServerEvents.OnUserLoggedIn(conn);
 		}
 
 		/// <summary>
@@ -121,6 +124,8 @@ namespace Melia.Barracks.Network
 			// Client closes connection without this as well, but it waits a
 			// few seconds to do so.
 			Send.BC_LOGOUTOK(conn);
+
+			BarracksServer.Instance.ServerEvents.OnUserLoggedOut(conn);
 		}
 
 		/// <summary>
@@ -147,6 +152,8 @@ namespace Melia.Barracks.Network
 			// to the client
 			//conn.Account.Properties.String("foobar").Value = "print('Hello, World!')";
 			//Send.BC_ACCOUNT_PROP(conn, conn.Account);
+
+			BarracksServer.Instance.ServerEvents.OnUserEntered(conn);
 		}
 
 		/// <summary>
@@ -344,10 +351,14 @@ namespace Melia.Barracks.Network
 			//character.SprByJob = jobData.Spr;
 			//character.DexByJob = jobData.Dex;
 
+			BarracksServer.Instance.ServerEvents.OnCreatingCharacter(new CharacterEventArgs(conn, character));
+
 			conn.Account.CreateCharacter(character);
 
 			Send.BC_COMMANDER_CREATE_SLOTID(conn, character);
 			Send.BC_COMMANDER_CREATE(conn, character);
+
+			BarracksServer.Instance.ServerEvents.OnCharacterCreated(new CharacterEventArgs(conn, character));
 		}
 
 		/// <summary>
@@ -379,6 +390,8 @@ namespace Melia.Barracks.Network
 
 			Send.BC_COMMANDER_DESTROY(conn, character.Index);
 			Send.BC_NORMAL.TeamUI(conn);
+
+			BarracksServer.Instance.ServerEvents.OnCharacterRemoved(new CharacterEventArgs(conn, character));
 		}
 
 		/// <summary>
