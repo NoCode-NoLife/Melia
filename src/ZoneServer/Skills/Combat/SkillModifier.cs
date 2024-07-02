193384bcf2686bafd3e982b3da1a281fb08b275c
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-07-02 16:02:10 -0400

    Daggerslash and cloak fixes
    Removing handlers for these buffs as they are unnecessary.
    Cloaking does not affect PDef / MDef, it just reduces final damage taken.  Daggerslash similarly mulipliers outgoing damage rather than buffing any stats.  It would be nice if there was some way to do this without putting it in Calc Combat.

---
+++ src/ZoneServer/Skills/Combat/SkillModifier.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Melia.Shared.Game.Const;
+
+namespace Melia.Zone.Skills.Combat
+{
+	/// <summary>
+	/// A class for properties that can modify the damage calculation for a skill
+	/// </summary>
+	public class SkillModifier
+	{
+		/// <summary>
+		/// Adds extra PAtk to the damage calculation
+		/// </summary>
+		public float BonusPAtk { get; set; }
+
+		/// <summary>
+		/// Adds extra MAtk to the damage calculation
+		/// </summary>
+		public float BonusMAtk { get; set; }
+
+		/// <summary>
+		/// Adds extra (flat) damage to the damage calculation
+		/// </summary>
+		public float BonusDamage { get; set; }
+
+		/// <summary>
+		/// Ignores a certain percentage of target's PDef and MDef
+		/// 0.1 = ignore 10% defense
+		/// </summary>
+		public float IgnoreDefPercent { get; set; }
+
+		/// <summary>
+		/// Multiplies the attack damage before enemy defense is applied
+		/// This is a percentage with 1.00 as the base, so 1.02 adds 2% damage
+		/// </summary>
+		public float DamageMultiplier { get; set; }
+
+		/// <summary>
+		/// The number of hits the skill does
+		/// This also multiplies the damage by this amount
+		/// </summary>
+		public int HitCount { get; set; }
+
+		/// <summary>
+		/// Creates new skill modifier.
+		/// </summary>
+		public SkillModifier()
+		{
+			this.DamageMultiplier = 1.0f;
+			this.HitCount = 1;
+		}
+	}
+}
