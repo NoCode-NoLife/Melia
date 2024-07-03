a98839af39b3098cf9f2fc6d41d3fb486aa1fdf7
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-07-03 01:08:17 -0400

    Behead Implemented
    Behead and its debuff are added.

---
+++ src/ZoneServer/Buffs/Handlers/Scout/Assassin/Behead_Debuff.cs
@@ -0,0 +1,28 @@
+﻿using Melia.Shared.Game.Const;
+using Melia.Zone.Buffs.Base;
+using Melia.Zone.Network;
+using Melia.Zone.Skills;
+using Melia.Zone.Skills.Combat;
+using Yggdrasil.Logging;
+
+namespace Melia.Zone.Buffs.Handlers
+{
+	/// <summary>
+	/// Buff handler for Behead: Bleeding, which deals damage
+	/// every 1.5 seconds
+	/// </summary>
+	[BuffHandler(BuffId.Behead_Debuff)]
+	public class Behead_Debuff : BuffHandler
+	{
+		public override void WhileActive(Buff buff)
+		{
+			if (!buff.Target.IsDead) { 
+				var skill = new Skill(buff.Caster, SkillId.Assassin_Behead_DOT, (int)buff.NumArg1);
+
+				buff.Target.TakeDamage(buff.NumArg2, buff.Caster);
+				var hit = new HitInfo(buff.Caster, buff.Target, skill, buff.NumArg2, HitResultType.Hit);
+				Send.ZC_HIT_INFO(buff.Caster, buff.Target, skill, hit);
+			}
+		}
+	}
+}
---
+++ src/ZoneServer/Skills/Handlers/Scout/Assassin/Behead.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Melia.Shared.Data.Database;
+using Melia.Shared.L10N;
+using Melia.Shared.Game.Const;
+using Melia.Shared.World;
+using Melia.Zone.Network;
+using Melia.Zone.Skills.Combat;
+using Melia.Zone.Skills.Handlers.Base;
+using Melia.Zone.Skills.SplashAreas;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.Characters.Components;
+using Melia.Zone.World.Actors.CombatEntities.Components;
+using static Melia.Zone.Skills.SkillUseFunctions;
+using Yggdrasil.Logging;
+
+namespace Melia.Zone.Skills.Handlers.Scout.Assassin
+{
+	/// <summary>
+	/// Handler for the Assassin skill Behead.
+	/// </summary>
+	[SkillHandler(SkillId.Assassin_Behead)]
+	public class Behead : IGroundSkillHandler
+	{
+		/// <summary>
+		/// Handles skill, damaging targets.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="originPos"></param>
+		/// <param name="farPos"></param>
+		public void Handle(Skill skill, ICombatEntity caster, Position originPos, Position farPos, ICombatEntity target)
+		{
+			if (target == null)
+			{
+				caster.ServerMessage(Localization.Get("No target."));
+				return;
+			}
+
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
+
+			// This skill always aims towards a target.  The farpos is always just the player's position
+			farPos = target.Position;
+
+			skill.IncreaseOverheat();
+			caster.TurnTowards(farPos);
+			caster.SetAttackState(true);
+
+			var splashParam = skill.GetSplashParameters(caster, originPos, farPos, length: 60, width: 35, angle: 0);
+			var splashArea = skill.GetSplashArea(SplashType.Square, splashParam);
+
+			Send.ZC_SKILL_READY(caster, skill, originPos, farPos);
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos, null);
+
+			this.Attack(skill, caster, splashArea);
+		}
+
+		/// <summary>
+		/// Executes the actual attack after a delay.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="splashArea"></param>
+		private async void Attack(Skill skill, ICombatEntity caster, ISplashArea splashArea)
+		{
+			var hitDelay = TimeSpan.FromMilliseconds(30);
+			var damageDelay1 = TimeSpan.FromMilliseconds(50);
+			var damageDelay2 = TimeSpan.FromMilliseconds(80);
+			var delayBetweenHits = TimeSpan.FromMilliseconds(330);
+			var skillHitDelay = TimeSpan.Zero;
+
+			await Task.Delay(hitDelay);
+
+			var targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
+			var hits = new List<SkillHitInfo>();
+
+			
+
+			foreach (var target in targets.LimitBySDR(caster, skill))
+			{
+				SkillModifier modifier = new SkillModifier();
+				modifier.HitCount = 3;
+				modifier.IgnoreDefPercent = 0.15f;
+
+				var skillHitResult = SCR_SkillHit(caster, target, skill, modifier);
+				target.TakeDamage(skillHitResult.Damage, caster);
+
+				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, damageDelay1, skillHitDelay);
+				skillHit.HitEffect = HitEffect.Impact;
+				hits.Add(skillHit);
+				Send.ZC_SKILL_HIT_INFO(caster, hits);
+			}
+
+			await Task.Delay(delayBetweenHits);
+			hits.Clear();
+
+			targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
+			foreach (var target in targets.LimitBySDR(caster, skill))
+			{
+				SkillModifier modifier = new SkillModifier();
+				modifier.HitCount = 3;
+				modifier.IgnoreDefPercent = 0.15f;
+
+				var skillHitResult2 = SCR_SkillHit(caster, target, skill, modifier);
+				target.TakeDamage(skillHitResult2.Damage, caster);
+
+				var skillHit2 = new SkillHitInfo(caster, target, skill, skillHitResult2, damageDelay2, skillHitDelay);
+				skillHit2.HitEffect = HitEffect.Impact;
+				hits.Add(skillHit2);
+				Send.ZC_SKILL_HIT_INFO(caster, hits);
+
+				// Behead Debuff deals 5% of the damage every 1.5s for 30s
+				target.StartBuff(BuffId.Behead_Debuff, skill.Level, skillHitResult2.Damage * 0.05f, TimeSpan.FromSeconds(30), caster);
+			}
+		}
+	}
+}
