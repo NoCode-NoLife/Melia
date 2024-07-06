b4048b0d544a0da04bacf1c8a12935b6266163b7
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-07-05 23:44:36 -0400

    Added Fulldraw and Heavy Shot
    The two removed skills for archer, along with the Hold status, which Fulldraw inflicts, are now implemented.

---
+++ src/ZoneServer/Buffs/Handlers/Hold.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Melia.Shared.Game.Const;
+using Melia.Zone.Buffs.Base;
+using Melia.Zone.Network;
+using Melia.Zone.World.Actors.CombatEntities.Components;
+using Yggdrasil.Logging;
+
+namespace Melia.Zone.Buffs.Handlers
+{
+	/// <summary>
+	/// Handle for the Hold Debuff, which prevents movement
+	/// </summary>
+	[BuffHandler(BuffId.Hold)]
+	public class Hold : BuffHandler
+	{
+		public override void OnStart(Buff buff)
+		{
+			var target = buff.Target;
+
+			if (target.Components.TryGet<MovementComponent>(out var movementComponent))
+			{
+				movementComponent.Stop();
+				movementComponent.SetFixedMoveSpeed(0.0001f);
+			}
+		}
+
+		public override void OnEnd(Buff buff)
+		{
+			var target = buff.Target;
+
+			if (target.Components.TryGet<MovementComponent>(out var movementComponent))
+			{
+				movementComponent.Stop();
+				movementComponent.ResetFixedMoveSpeed();
+			}
+		}
+	}
+}
---
+++ src/ZoneServer/Skills/Handlers/Archer/Fulldraw.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Melia.Shared.L10N;
+using Melia.Shared.Game.Const;
+using Melia.Shared.World;
+using Melia.Zone.Network;
+using Melia.Zone.Skills.Combat;
+using Melia.Zone.Skills.Handlers.Base;
+using Melia.Zone.Skills.SplashAreas;
+using Melia.Zone.World.Actors;
+using Melia.Zone.World.Actors.Characters;
+using Melia.Zone.World.Actors.CombatEntities.Components;
+using Yggdrasil.Extensions;
+using Yggdrasil.Logging;
+using static Melia.Zone.Skills.SkillUseFunctions;
+using Melia.Shared.Data.Database;
+
+namespace Melia.Zone.Skills.Handlers.Archer
+{
+	/// <summary>
+	/// Handler for the Archer skill Multishot.
+	/// </summary>
+	[SkillHandler(SkillId.Archer_Fulldraw)]
+	public class Fulldraw : ITargetSkillHandler, IDynamicCasted
+	{
+		/// <summary>
+		/// Called when the user starts casting the skill.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		public void StartDynamicCast(Skill skill, ICombatEntity caster)
+		{
+			Send.ZC_PLAY_SOUND(caster, "voice_war_atk_long_cast");
+		}
+
+		/// <summary>
+		/// Called when the user stops casting the skill.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		public void EndDynamicCast(Skill skill, ICombatEntity caster)
+		{
+			Send.ZC_STOP_SOUND(caster, "voice_war_atk_long_cast");
+		}
+
+		/// <summary>
+		/// Handles skill, damaging targets.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="originPos"></param>
+		/// <param name="farPos"></param>
+		public void Handle(Skill skill, ICombatEntity caster, ICombatEntity target)
+		{
+			if (!caster.TrySpendSp(skill))
+			{
+				caster.ServerMessage(Localization.Get("Not enough SP."));
+				return;
+			}
+
+			var originPos = caster.Position;
+			var farPos = target.Position;
+
+			skill.IncreaseOverheat();
+			caster.TurnTowards(farPos);
+			caster.SetAttackState(true);
+
+			var splashParam = skill.GetSplashParameters(caster, originPos, farPos, length: 150, width: 10, angle: 0);
+			var splashArea = skill.GetSplashArea(SplashType.Square, splashParam);
+
+			Send.ZC_SKILL_READY(caster, skill, originPos, farPos);
+			Send.ZC_SKILL_MELEE_GROUND(caster, skill, farPos, null);
+			
+			this.Attack(skill, caster, splashArea);
+		}
+
+		/// <summary>
+		/// Executes the actual attack after a delay.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <param name="caster"></param>
+		/// <param name="splashArea"></param>
+		private async void Attack(Skill skill, ICombatEntity caster, ISplashArea splashArea)
+		{
+			var hitDelay = TimeSpan.FromMilliseconds(100);
+			var damageDelay = TimeSpan.FromMilliseconds(100);
+			var skillHitDelay = TimeSpan.Zero;
+
+			await Task.Delay(hitDelay);
+
+			var targets = caster.Map.GetAttackableEntitiesIn(caster, splashArea);
+			var skillHits = new List<SkillHitInfo>();
+
+			foreach (var target in targets.LimitBySDR(caster, skill))
+			{
+				var skillHitResult = SCR_SkillHit(caster, target, skill);
+				target.TakeDamage(skillHitResult.Damage, caster);
+
+				var skillHit = new SkillHitInfo(caster, target, skill, skillHitResult, damageDelay, skillHitDelay);
+				skillHits.Add(skillHit);
+
+				var holdDuration = 5 + skill.Level;
+
+				target.StartBuff(BuffId.Hold, skill.Level, 0, TimeSpan.FromSeconds(holdDuration), caster);				
+			}
+
+			Send.ZC_SKILL_HIT_INFO(caster, skillHits);
+		}
+	}
+}
