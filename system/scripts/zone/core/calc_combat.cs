be18ad011fc7c16f1780c49ea9f4d12a9856c14b
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-07-08 17:34:30 -0400

    Fix for Cloaking Formula
    See PR comments

--- system/scripts/zone/core/calc_combat.cs
+++ system/scripts/zone/core/calc_combat.cs
@@ -176,7 +176,7 @@ public class CombatCalculationsScript : GeneralScript
 		if (target.Components.Get<BuffComponent>().TryGet(BuffId.Cloaking_Buff, out var cloakingBuff))
 		{
 			// Cloaking reduces damage by 25%
-			damage = Math.Max(1, damage - damage * 0.75f);
+			damage = Math.Max(1, damage - damage * 0.25f);
 		}
     
 		// Block damage reduction
