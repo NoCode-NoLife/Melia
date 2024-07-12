271bc9bcbdd59962acda25f8fb644fa8144f2df7
Exec Tails <exec@dragonbyte.de>
2024-07-12 16:09:07 +0200

    Added Hasisas potion usage and HP loss options

--- src/ZoneServer/Buffs/Handlers/Scout/Assassin/Hasisas.cs
+++ src/ZoneServer/Buffs/Handlers/Scout/Assassin/Hasisas.cs
@@ -1,6 +1,6 @@
-﻿using System.Reflection.Metadata;
-using Melia.Shared.Game.Const;
+﻿using Melia.Shared.Game.Const;
 using Melia.Zone.Buffs.Base;
+using Melia.Zone.World.Actors.Characters;
 
 namespace Melia.Zone.Buffs.Handlers.Scout.Assassin
 {
@@ -12,10 +12,12 @@ namespace Melia.Zone.Buffs.Handlers.Scout.Assassin
 	{
 		private const string AspdVarName = "Melia.AttackSpeedBonus";
 		private const string CritVarName = "Melia.CritDamageBonus";
+
 		private const float AspdBonusBase = 155;
 		private const float AspdBonusPerLevel = 20;
 		private const float CritBonusBase = 3f;
 		private const float CritBonusPerLevel = 1.5f;
+		private const float HpLossRate = 0.01f;
 
 		public override void OnStart(Buff buff)
 		{
@@ -36,6 +38,31 @@ namespace Melia.Zone.Buffs.Handlers.Scout.Assassin
 
 			if (buff.Vars.TryGetFloat(CritVarName, out var critBonus))
 				buff.Target.Properties.Modify(PropertyName.CRTATK_BM, -critBonus);
+		}
+
+		public override void WhileActive(Buff buff)
+		{
+			if (Feature.IsEnabled("HasisasNoHpLoss"))
+				return;
+
+			var target = buff.Target;
+
+			// The description stats an HP loss of 1% per 10 seconds,
+			// which matches the buff's update time. Should a user
+			// change it, the description would no longer be accurate,
+			// but we'll leave it like this for now because we have
+			// no great way of keeping track of passage of time.
+			// TODO: Pass time passed into WhileActive.
+
+			var maxHp = target.Properties.GetFloat(PropertyName.MHP);
+			var loss = maxHp * HpLossRate;
+
+			// TODO: We probably don't need handling for monsters,
+			//   but this should still get updated once we have a
+			//   general HP modifier. Or perhaps it should be a
+			//   damage hit? TBD.
+			if (target is Character character)
+				character.ModifyHp(-loss);
 		}
 	}
 }
--- src/ZoneServer/Skills/Handlers/Scout/Assassin/Hasisas.cs
+++ src/ZoneServer/Skills/Handlers/Scout/Assassin/Hasisas.cs
@@ -10,15 +10,19 @@ using Melia.Zone.Network;
 using Melia.Zone.Skills.Handlers.Base;
 using Melia.Zone.World.Actors;
 using Melia.Zone.World.Actors.CombatEntities.Components;
+using Melia.Zone.World.Actors.Characters.Components;
 
 namespace Melia.Zone.Skills.Handlers.Scout.Assassin
 {
 	/// <summary>
-	/// Handler for the Assassin skill Hasisas
+	/// Handler for the Assassin skill Hasisas.
 	/// </summary>
 	[SkillHandler(SkillId.Assassin_Hasisas)]
 	public class Hasisas : IGroundSkillHandler
 	{
+		private const int HasisasPotionId = 647010;
+		private static readonly TimeSpan BuffDuration = TimeSpan.FromMinutes(30);
+
 		/// <summary>
 		/// Handles skill, applying the buff to the caster.
 		/// </summary>
@@ -34,15 +38,39 @@ namespace Melia.Zone.Skills.Handlers.Scout.Assassin
 				return;
 			}
 
+			if (!this.UsePotion(caster))
+			{
+				caster.ServerMessage(Localization.Get("You need a Hasisas Potion."));
+				return;
+			}
+
 			skill.IncreaseOverheat();
 			caster.SetAttackState(true);
 
-			target = caster;
+			caster.StartBuff(BuffId.Hasisas_Buff, skill.Level, 0, BuffDuration, caster);
 
-			var duration = TimeSpan.FromMinutes(30);
-			target.StartBuff(BuffId.Hasisas_Buff, skill.Level, 0, duration, caster);
+			Send.ZC_SKILL_MELEE_TARGET(caster, skill, caster, null);
+		}
 
-			Send.ZC_SKILL_MELEE_TARGET(caster, skill, target, null);
+		/// <summary>
+		/// Uses one Hasisas Potion if applicable. Returns false if consumption
+		/// failed and the skill should not be used.
+		/// </summary>
+		/// <param name="caster"></param>
+		/// <returns></returns>
+		private bool UsePotion(ICombatEntity caster)
+		{
+			if (Feature.IsEnabled("HasisasNoPotion"))
+				return true;
+
+			if (caster.Components.TryGet<InventoryComponent>(out var inventory))
+			{
+				var removedCount = inventory.Remove(HasisasPotionId, 1, InventoryItemRemoveMsg.Used);
+				if (removedCount == 0)
+					return false;
+			}
+
+			return true;
 		}
 	}
 }
