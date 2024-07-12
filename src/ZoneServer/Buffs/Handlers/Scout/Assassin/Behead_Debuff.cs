1ecf332db7638f269d58ddf64d68bd05fbfad65a
Exec Tails <exec@dragonbyte.de>
2024-07-12 15:02:05 +0200

    Minor cleanup of Behead handler

--- src/ZoneServer/Buffs/Handlers/Scout/Assassin/Behead_Debuff.cs
+++ src/ZoneServer/Buffs/Handlers/Scout/Assassin/Behead_Debuff.cs
@@ -1,25 +1,28 @@
 ﻿using Melia.Shared.Game.Const;
 using Melia.Zone.Buffs.Base;
 using Melia.Zone.Network;
-using Melia.Zone.Skills;
 using Melia.Zone.Skills.Combat;
-using Yggdrasil.Logging;
 
 namespace Melia.Zone.Buffs.Handlers.Scout.Assassin
 {
 	/// <summary>
-	/// Buff handler for Behead: Bleeding, which deals damage
-	/// every 1.5 seconds
+	/// Buff handler for Behead: Bleeding, which deals damage in regular intervals.
 	/// </summary>
 	[BuffHandler(BuffId.Behead_Debuff)]
 	public class Behead_Debuff : BuffHandler
 	{
 		public override void WhileActive(Buff buff)
 		{
-			if (!buff.Target.IsDead) { 
-				buff.Target.TakeDamage(buff.NumArg2, buff.Caster);
-				var hit = new HitInfo(buff.Caster, buff.Target, null, buff.NumArg2, HitResultType.Hit);
-				Send.ZC_HIT_INFO(buff.Caster, buff.Target, SkillId.Assassin_Behead_DOT, hit);
+			if (!buff.Target.IsDead)
+			{
+				var attacker = buff.Caster;
+				var target = buff.Target;
+				var damage = buff.NumArg2;
+
+				target.TakeDamage(damage, attacker);
+
+				var hit = new HitInfo(attacker, target, null, damage, HitResultType.Hit);
+				Send.ZC_HIT_INFO(attacker, target, SkillId.Assassin_Behead_DOT, hit);
 			}
 		}
 	}
