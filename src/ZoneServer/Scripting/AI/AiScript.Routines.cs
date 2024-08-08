267361e8ef8e25b2d654b3a3a14df84ea9b0dd50
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-08-08 00:38:41 -0400

    Making AI Routines Virtual
    I need these two methods to be virtual for my custom package to work.

--- src/ZoneServer/Scripting/AI/AiScript.Routines.cs
+++ src/ZoneServer/Scripting/AI/AiScript.Routines.cs
@@ -131,7 +131,7 @@ namespace Melia.Zone.Scripting.AI
 		/// </summary>
 		/// <param name="skill"></param>
 		/// <returns></returns>
-		protected bool TryGetRandomSkill(out Skill skill)
+		protected virtual bool TryGetRandomSkill(out Skill skill)
 		{
 			skill = null;
 
@@ -165,7 +165,7 @@ namespace Melia.Zone.Scripting.AI
 		/// <param name="skill"></param>
 		/// <param name="target"></param>
 		/// <returns></returns>
-		protected IEnumerable UseSkill(Skill skill, ICombatEntity target)
+		protected virtual IEnumerable UseSkill(Skill skill, ICombatEntity target)
 		{
 			this.Entity.TurnTowards(target);
 
