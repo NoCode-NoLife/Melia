b945eb62ad4c61174d33a44d3e3d421c2108c859
Exec Tails <exec@dragonbyte.de>
2024-08-11 22:54:39 +0200

    Fixed PCEtc id

--- src/ZoneServer/World/Actors/Characters/PCEtc.cs
+++ src/ZoneServer/World/Actors/Characters/PCEtc.cs
@@ -7,10 +7,12 @@ namespace Melia.Zone.World.Actors.Characters
 	/// </summary>
 	public class PCEtc : IPropertyObject
 	{
+		private Character _character;
+
 		/// <summary>
 		/// Returns the property object's unique id.
 		/// </summary>
-		public long ObjectId { get; }
+		public long ObjectId => _character.SocialUserId;
 
 		/// <summary>
 		/// Returns the object's properties.
@@ -23,7 +25,9 @@ namespace Melia.Zone.World.Actors.Characters
 		/// <param name="character"></param>
 		public PCEtc(Character character)
 		{
-			this.ObjectId = character.SocialUserId;
+			// Since the db id is not set at time of creation of this object,
+			// we save the character and reference the id dynamically.
+			_character = character;
 		}
 	}
 }
