a948ab6b55654292589016acd071dce794f9d170
Exec Tails <exec@dragonbyte.de>
2024-09-20 14:00:31 +0200

    Updated friend info packet slightly

--- src/SocialServer/Network/Helpers/Friend.cs
+++ src/SocialServer/Network/Helpers/Friend.cs
@@ -18,26 +18,35 @@ namespace Melia.Social.Network.Helpers
 			var user = friend.User;
 			var character = user.Character;
 
-			packet.PutLong(0);
-			packet.PutLong(user.Id);
-			packet.PutString(user.TeamName, 64);
-			packet.PutString("", 56);
-			packet.PutLong(0);
-			packet.PutInt(character.Level);
+			// Friend
+			{
+				packet.PutLong(0);
+				packet.PutLong(user.Id);
+				packet.PutString(user.TeamName, 64);
+				packet.PutString("", 56);
+				packet.PutLong(0);
+				packet.PutInt(character.Level);
 
-			packet.PutString(character.TeamName, 64);
-			packet.PutString(character.Name, 64);
-			packet.PutShort(0);
-			packet.PutInt((int)character.JobId);
-			packet.PutShort(0);
-			packet.PutInt(0);
-			packet.PutShort((short)character.Gender);
-			packet.PutInt(character.Hair);
-			packet.PutEmptyBin(26);
-			packet.PutUInt(0xFF808080); // either not skin color or wrong place?
-			packet.PutEmptyBin(18);
-			packet.PutShortDate(user.LastLogin);
-			packet.PutEmptyBin(33);
+				// Appearance
+				{
+					packet.PutString(character.TeamName, 64);
+					packet.PutString(character.Name, 64);
+					packet.PutShort(0);
+					packet.PutShort((int)character.JobId);
+					packet.PutInt(0);
+					packet.PutInt(0);
+					packet.PutShort((short)character.Gender);
+					packet.PutInt(character.Hair);
+					packet.PutEmptyBin(26);
+					packet.PutUInt(0xFF808080); // either not skin color or wrong place?
+					packet.PutEmptyBin(16);
+				}
+
+				packet.PutShort(0);
+				packet.PutShortDate(user.LastLogin);
+				packet.PutString("", 32);
+				packet.PutByte(0);
+			}
 
 			packet.PutShort(character.MapId);
 			packet.PutShort(character.ChannelId);
