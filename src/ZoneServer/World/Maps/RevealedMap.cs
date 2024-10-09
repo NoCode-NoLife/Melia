452ff37e40857cf5ee2ac386c8270a7d69b75d5f
Exec Tails <exec@dragonbyte.de>
2024-10-09 14:53:30 +0200

    Minor changes in revealed map class

--- src/ZoneServer/World/Maps/RevealedMap.cs
+++ src/ZoneServer/World/Maps/RevealedMap.cs
@@ -1,22 +1,25 @@
 ﻿namespace Melia.Zone.World.Maps
 {
+	/// <summary>
+	/// Information about a player's exploration progress on a map.
+	/// </summary>
 	public class RevealedMap
 	{
 		/// <summary>
 		/// Gets or sets the class id of the map.
 		/// </summary>
-		public int MapId { get; set; }
+		public int MapId { get; }
 
 		/// <summary>
 		/// Gets or sets an array of 128 bytes where each bit represents an
 		/// explored sector of a map.
 		/// </summary>
-		public byte[] Explored { get; set; }
+		public byte[] Explored { get; private set; }
 
 		/// <summary>
 		/// Gets or sets the percentage of the entire map that has been revealed.
 		/// </summary>
-		public float Percentage { get; set; }
+		public float Percentage { get; private set; }
 
 		/// <summary>
 		/// Creates new instance.
