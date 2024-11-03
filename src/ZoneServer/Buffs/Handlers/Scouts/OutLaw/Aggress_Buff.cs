12deb846dfa650f56a97f730a82144a586e6cfb7
Terotrous <139150104+Terotrous@users.noreply.github.com>
2024-11-03 18:48:10 -0500

    Readding files

---
+++ src/ZoneServer/Buffs/Handlers/Scouts/OutLaw/Aggress_Buff.cs
@@ -0,0 +1,37 @@
+﻿using Melia.Shared.Game.Const;
+using Melia.Zone.Buffs.Base;
+using Melia.Zone.Network;
+
+namespace Melia.Zone.Buffs.Handlers.Scouts.OutLaw
+{
+	/// <summary>
+	/// Handler for Aggress Buff, which massively increases movement speed
+	/// </summary>
+	[BuffHandler(BuffId.Aggress_Buff)]
+	public class Aggress_Buff : BuffHandler
+	{
+		private const float MspdBonus = 30f;
+
+		/// <summary>
+		/// Starts buff, modifying the movement speed
+		/// </summary>
+		/// <param name="buff"></param>
+		public override void OnActivate(Buff buff, ActivationType activationType)
+		{
+			var target = buff.Target;
+
+			AddPropertyModifier(buff, target, PropertyName.MSPD_BM, MspdBonus);
+			Send.ZC_MSPD(target);
+		}
+
+		/// <summary>
+		/// Ends the buff, resetting the movement speed
+		/// </summary>
+		/// <param name="buff"></param>
+		public override void OnEnd(Buff buff)
+		{
+			RemovePropertyModifier(buff, buff.Target, PropertyName.MSPD_BM);
+			Send.ZC_MSPD(buff.Target);
+		}
+	}
+}
