363bc845daab846be73edc5613fd97e3e0557d02
Exec Tails <exec@dragonbyte.de>
2025-05-25 23:52:15 +0200

    Added override look up for scriptable functions
    Basic support for getting the original function in cases where scripts override existing scriptable functions. For example, a custom script might override a skill calculation function to modify the result passed to the server. With access to the original function, the custom script can retrieve the original return value and apply changes to it, such as multiplying it, without needing to recreate the function or even know what the original did.

--- src/ZoneServer/Scripting/DelegateCollection.cs
+++ src/ZoneServer/Scripting/DelegateCollection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 
 namespace Melia.Zone.Scripting
@@ -27,7 +28,10 @@ namespace Melia.Zone.Scripting
 	/// <typeparam name="TDelegate"></typeparam>
 	public class DelegateCollection<TDelegate> : IDelegateCollection where TDelegate : Delegate
 	{
+		private readonly object _syncLock = new();
+
 		private readonly Dictionary<string, TDelegate> _funcs = new();
+		private readonly Dictionary<object, FunctionOverride<TDelegate>> _overrides = new();
 
 		/// <summary>
 		/// Registers the function with the given name.
@@ -36,8 +40,16 @@ namespace Melia.Zone.Scripting
 		/// <param name="func"></param>
 		public void Register(string funcName, TDelegate func)
 		{
-			lock (_funcs)
+			lock (_syncLock)
+			{
+				if (_funcs.TryGetValue(funcName, out var overriddenFunc))
+				{
+					var funcOverride = new FunctionOverride<TDelegate>(funcName, overriddenFunc, func);
+					_overrides[func] = funcOverride;
+				}
+
 				_funcs[funcName] = func;
+			}
 		}
 
 		/// <summary>
@@ -66,7 +78,7 @@ namespace Melia.Zone.Scripting
 		/// </exception>
 		public TDelegate Get(string funcName)
 		{
-			lock (_funcs)
+			lock (_syncLock)
 			{
 				if (!_funcs.TryGetValue(funcName, out var func))
 					throw new ArgumentException($"Function '{funcName}' not found.");
@@ -84,8 +96,36 @@ namespace Melia.Zone.Scripting
 		/// <returns></returns>
 		public bool TryGet(string funcName, out TDelegate func)
 		{
-			lock (_funcs)
+			lock (_syncLock)
 				return _funcs.TryGetValue(funcName, out func);
+		}
+
+		/// <summary>
+		/// Looks up a function that was overridden by the given function and
+		/// returns it via out. Returns false if no such function was found.
+		/// </summary>
+		/// <remarks>
+		/// If a function is registered with the same name as an existing function,
+		/// a reference to the overriden function is stored and can be retrieved
+		/// via this method. This allows overriding functions to access the original
+		/// function, its functionality, and its results.
+		/// </remarks>
+		/// <param name="func"></param>
+		/// <param name="overriddenFunc"></param>
+		/// <returns></returns>
+		public bool TryGetOverridden(TDelegate func, out TDelegate overriddenFunc)
+		{
+			lock (_syncLock)
+			{
+				if (_overrides.TryGetValue(func, out var funcOverride))
+				{
+					overriddenFunc = funcOverride.OverriddenFunc;
+					return true;
+				}
+			}
+
+			overriddenFunc = null;
+			return false;
 		}
 
 		/// <summary>
@@ -94,8 +134,14 @@ namespace Melia.Zone.Scripting
 		/// <param name="funcName"></param>
 		public void Remove(string funcName)
 		{
-			lock (_funcs)
+			lock (_syncLock)
+			{
 				_funcs.Remove(funcName);
+
+				var overrides = _overrides.Values.Where(a => a.Name == funcName);
+				foreach (var overrideEntry in overrides)
+					_overrides.Remove(overrideEntry.NewFunc);
+			}
 		}
 
 		/// <summary>
@@ -103,8 +149,11 @@ namespace Melia.Zone.Scripting
 		/// </summary>
 		public void Clear()
 		{
-			lock (_funcs)
+			lock (_syncLock)
+			{
 				_funcs.Clear();
+				_overrides.Clear();
+			}
 		}
 
 		/// <summary>
@@ -153,6 +202,50 @@ namespace Melia.Zone.Scripting
 
 			func = (TDelegate)Delegate.CreateDelegate(typeof(TDelegate), null, method);
 			return true;
+		}
+	}
+
+	/// <summary>
+	/// Information about a function override.
+	/// </summary>
+	/// <typeparam name="TDelegate"></typeparam>
+	internal class FunctionOverride<TDelegate> where TDelegate : Delegate
+	{
+		/// <summary>
+		/// Returns the shared reference name of the function that was overridden.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Returns a reference to the original function that was overridden.
+		/// </summary>
+		public TDelegate OverriddenFunc { get; set; }
+
+		/// <summary>
+		/// Returns a reference to the new function that overrides the original one.
+		/// </summary>
+		public TDelegate NewFunc { get; set; }
+
+		/// <summary>
+		/// Creates new instance.
+		/// </summary>
+		/// <param name="name"></param>
+		/// <param name="originalFunc"></param>
+		/// <param name="newFunc"></param>
+		public FunctionOverride(string name, TDelegate originalFunc, TDelegate newFunc)
+		{
+			this.Name = name;
+			this.OverriddenFunc = originalFunc;
+			this.NewFunc = newFunc;
+		}
+
+		/// <summary>
+		/// Returns a string representation of the function override,
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return $"{this.NewFunc.Method.Name} : {this.OverriddenFunc.Method.Name} ({this.Name})";
 		}
 	}
 }
