32a89bfb6aeb839163ca44910a94a77e1cd6695e
Exec Tails <exec@dragonbyte.de>
2025-05-29 16:58:36 +0200

    Added web API info endpoint for world information

--- src/WebServer/Controllers/Api/InfoController.cs
+++ src/WebServer/Controllers/Api/InfoController.cs
@@ -15,11 +15,53 @@ namespace Melia.Web.Controllers.Api
 	internal class InfoController : JsonApiController
 	{
 		/// <summary>
+		/// Handles requests for retrieving general server information.
+		/// </summary>
+		/// <returns></returns>
+		[Route(HttpVerbs.Get, "/world")]
+		public async Task World()
+		{
+			var comm = WebServer.Instance.Communicator;
+			var commMessage = new ReqServerListMessage();
+
+			var responseMsg = await comm.RequestResponse<ResServerListMessage>("Coordinator", commMessage);
+			if (responseMsg == null)
+			{
+				await this.Error("Failed to communicate with coordinator.");
+				return;
+			}
+
+			// We'll consider the server online if at least one barracks and one
+			// zone server are online, indicating that you can log in and play.
+			var barracksOnline = responseMsg.Servers.Any(server => server.Type == ServerType.Barracks);
+			var zonesOnline = responseMsg.Servers.Any(server => server.Type == ServerType.Zone);
+			var serverOnline = barracksOnline && zonesOnline;
+
+			// Player count for gauging server activity
+			var playersOnline = responseMsg.Servers.Sum(server => server.CurrentPlayers);
+			var playersMax = responseMsg.Servers.Sum(server => server.MaxPlayers);
+
+			// World settings for information such as EXP and drop rates,
+			// as well as other customizations.
+			var worldSettings = WebServer.Instance.Conf.World;
+
+			await this.Ok(new
+			{
+				result = ApiResults.Success,
+
+				serverOnline,
+				playersOnline,
+				playersMax,
+				worldSettings,
+			});
+		}
+
+		/// <summary>
 		/// Handles requests for retrieving the list of servers.
 		/// </summary>
 		/// <returns></returns>
 		[Route(HttpVerbs.Get, "/servers")]
-		public async Task GetServers()
+		public async Task Servers()
 		{
 			var comm = WebServer.Instance.Communicator;
 			var commMessage = new ReqServerListMessage();
