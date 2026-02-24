835a56253e809705fdcf1d7d87b644703be5a99e
Exec Tails <exec@dragonbyte.de>
2026-02-24 01:30:58 +0100

    Added experimental new handling for calc events
    Our current approach to hooking into the combat calculations from skills, abilities, and buffs is clean, but without studying the source code it's very difficult to follow it, since a lot of magic is happening behind the scenes. This change would align these event methods more with what they actually are: scriptable functions. It would somewhat unify the look and feel of these methods, regardless of where they're used and defined, and also simplify the signatures and calling processes.

--- src/ZoneServer/Skills/Handlers/Archers/Ranger/Ranger_SteadyAim.cs
+++ src/ZoneServer/Skills/Handlers/Archers/Ranger/Ranger_SteadyAim.cs
@@ -1,4 +1,5 @@
 ﻿using Melia.Shared.Game.Const;
+using Melia.Zone.Scripting.ScriptableEvents;
 using Melia.Zone.Skills.Combat;
 using Melia.Zone.Skills.Handlers.Base;
 using Melia.Zone.World.Actors;
@@ -7,35 +8,38 @@ using Melia.Zone.World.Actors.Characters.Components;
 namespace Melia.Zone.Skills.Handlers.Archers.Ranger
 {
 	/// <summary>
-	/// Handler for the Ranger skill Full Throttle
+	/// Handler for the Ranger skill Full Throttle.
 	/// </summary>
 	/// <remarks>
 	/// This skill repurposes the skill id of an earlier skill
 	/// called Steady Aim, though the effect is completely different.
 	/// </remarks>
 	[SkillHandler(SkillId.Ranger_SteadyAim)]
-	public class Ranger_SteadyAim : ISkillHandler, ISkillCombatAttackBeforeCalcHandler, ISkillCombatAttackAfterBonusesHandler
+	public class Ranger_SteadyAim : ISkillHandler
 	{
 		/// <summary>
 		/// Applies the skill's effect during combat calculations.
 		/// </summary>
-		/// <param name="skill"></param>
 		/// <param name="attacker"></param>
 		/// <param name="target"></param>
 		/// <param name="attackerSkill"></param>
 		/// <param name="modifier"></param>
 		/// <param name="skillHitResult"></param>
-		public void OnAttackBeforeCalc(Skill skill, ICombatEntity attacker, ICombatEntity target, Skill attackerSkill, SkillModifier modifier, SkillHitResult skillHitResult)
+		[CombatCalcModifier(CombatCalcPhase.BeforeCalc, SkillId.Ranger_SteadyAim)]
+		public float OnCombatBeforeCalc(ICombatEntity attacker, ICombatEntity target, Skill attackerSkill, SkillModifier modifier, SkillHitResult skillHitResult)
 		{
 			// Ranger47 completely changes the bonus
 			if (!attacker.IsAbilityActive(AbilityId.Ranger47))
-				return;
+				return 0;
+
+			if (!attacker.TryGetSkill(SkillId.Ranger_SteadyAim, out var skill))
+				return 0;
 
 			var inventory = attacker.Components.Get<InventoryComponent>();
 			var weapon = inventory?.GetItem(EquipSlot.RightHand);
 
 			if (weapon == null)
-				return;
+				return 0;
 
 			var multiplierBonus = 0.18f + skill.Level * 0.03f + 0.65f;
 
@@ -44,22 +48,27 @@ namespace Melia.Zone.Skills.Handlers.Archers.Ranger
 				case EquipType.THBow: modifier.HitRateMultiplier += multiplierBonus; break;
 				case EquipType.Bow: modifier.BlockPenetrationMultiplier += multiplierBonus; break;
 			}
+
+			return 0;
 		}
 
 		/// <summary>
 		/// Applies the skill's effect during combat calculations.
 		/// </summary>
-		/// <param name="skill"></param>
 		/// <param name="attacker"></param>
 		/// <param name="target"></param>
 		/// <param name="attackerSkill"></param>
 		/// <param name="modifier"></param>
 		/// <param name="skillHitResult"></param>
-		public void OnAttackAfterBonuses(Skill skill, ICombatEntity attacker, ICombatEntity target, Skill attackerSkill, SkillModifier modifier, SkillHitResult skillHitResult)
+		[CombatCalcModifier(CombatCalcPhase.AfterBonuses, SkillId.Ranger_SteadyAim)]
+		public float OnCombatAfterBonuses(ICombatEntity attacker, ICombatEntity target, Skill attackerSkill, SkillModifier modifier, SkillHitResult skillHitResult)
 		{
 			// Ranger47 completely changes the bonus
 			if (attacker.IsAbilityActive(AbilityId.Ranger47))
-				return;
+				return 0;
+
+			if (!attacker.TryGetSkill(SkillId.Ranger_SteadyAim, out var skill))
+				return 0;
 
 			var isCrit = skillHitResult.Result == HitResultType.Crit;
 			var isRangerAttackSkill = attackerSkill.Id >= SkillId.Ranger_Barrage && attackerSkill.Id <= SkillId.Ranger_BlazingArrow;
@@ -69,6 +78,8 @@ namespace Melia.Zone.Skills.Handlers.Archers.Ranger
 				var multiplierBonus = 0.18f + skill.Level * 0.03f;
 				modifier.FinalDamageMultiplier += multiplierBonus;
 			}
+
+			return 0;
 		}
 	}
 }
