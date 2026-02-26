68686115704e1e98a04b1ee5233dc93cf30ffee5
Exec Tails <exec@dragonbyte.de>
2026-02-26 17:37:42 +0100

    Fixed result of IncreaseOverheat
    It's supposed to return true if the skill did overheat on that call.

--- src/ZoneServer/Skills/Skill.cs
+++ src/ZoneServer/Skills/Skill.cs
@@ -194,7 +194,7 @@ namespace Melia.Zone.Skills
 			{
 				this.OverheatCounter = 0;
 				this.OverheatTimeRemaining = TimeSpan.Zero;
-				overheated = false;
+				overheated = true;
 
 				this.Owner.Components.Get<CooldownComponent>().Start(this.Data.CooldownGroup, this.Data.CooldownTime);
 			}
