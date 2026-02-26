519860f11b62e1d09c103c5bfd9eb639798e3ad8
Exec Tails <exec@dragonbyte.de>
2026-02-26 17:55:48 +0100

    Experimental change to Teleportation
    Moved the max overheat calculation to a scriptable function.

---
+++ src/ZoneServer/Scripting/ScriptableEvents/SkillPropertyModifiers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Melia.Shared.Game.Const;
+
+namespace Melia.Zone.Scripting.ScriptableEvents
+{
+	/// <summary>
+	/// Declares a scriptable function with the name "SCR_Get_SpendSP_"
+	/// followed by the skill id, which overrides the SP cost calculations
+	/// for the specified skill.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SkillSpModifierAttribute : ScriptableFunctionAttribute
+	{
+		public SkillSpModifierAttribute(SkillId skillId)
+			: base("SCR_Get_SpendSP_" + skillId)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Declares a scriptable function with the name
+	/// "GET_SKILL_OVERHEAT_COUNT_" followed by the skill id, which
+	/// overrides the max overheat count calculations for the specified
+	/// skill.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SkillOverbuffMaxCountModifierAttribute : ScriptableFunctionAttribute
+	{
+		public SkillOverbuffMaxCountModifierAttribute(SkillId skillId)
+			: base("GET_SKILL_OVERHEAT_COUNT_" + skillId)
+		{
+		}
+	}
+
+	/// <summary>
+	/// Declares a scriptable function with the name
+	/// "SCR_GET_USEOVERHEAT_" followed by the skill id, which overrides
+	/// the overheat cooldown calculations for the specified skill.
+	/// </summary>
+	[AttributeUsage(AttributeTargets.Method)]
+	public class SkillOverbuffCooldownModifierAttribute : ScriptableFunctionAttribute
+	{
+		public SkillOverbuffCooldownModifierAttribute(SkillId skillId)
+			: base("SCR_GET_USEOVERHEAT_" + skillId)
+		{
+		}
+	}
+}
--- src/ZoneServer/Skills/Handlers/Wizards/Wizard/Wizard_Teleportation.cs
+++ src/ZoneServer/Skills/Handlers/Wizards/Wizard/Wizard_Teleportation.cs
@@ -3,6 +3,7 @@ using Melia.Shared.Game.Const;
 using Melia.Shared.L10N;
 using Melia.Shared.World;
 using Melia.Zone.Network;
+using Melia.Zone.Scripting.ScriptableEvents;
 using Melia.Zone.Skills.Handlers.Base;
 using Melia.Zone.World.Actors;
 using Melia.Zone.World.Actors.Characters.Components;
@@ -34,14 +35,7 @@ namespace Melia.Zone.Skills.Handlers.Wizards.Wizard
 				return;
 			}
 
-			// Increase max overheat if "Teleportation: Return" is active,
-			// which gives the user a brief window during which they can
-			// teleport back to the position they teleported from.
-			var overheatMaxCount = 1;
-			if (caster.IsAbilityActive(AbilityId.Wizard30))
-				overheatMaxCount = 2;
-
-			skill.IncreaseOverheat(overheatMaxCount);
+			skill.IncreaseOverheat();
 			caster.SetAttackState(true);
 
 			caster.StartBuff(BuffId.Teleportation_Buff, 0, 0, TimeSpan.FromSeconds(1), caster);
@@ -71,6 +65,25 @@ namespace Melia.Zone.Skills.Handlers.Wizards.Wizard
 
 			caster.Position = targetPos;
 			Send.ZC_SET_POS(caster, targetPos);
+		}
+
+		/// <summary>
+		/// Returns the skill's max overheat count.
+		/// </summary>
+		/// <param name="skill"></param>
+		/// <returns></returns>
+		[SkillOverbuffMaxCountModifier(SkillId.Wizard_Teleportation)]
+		public float GetOverheatMaxCount(Skill skill)
+		{
+			var result = 1;
+
+			// Increase max overheat if "Teleportation: Return" is active,
+			// which gives the user a brief window during which they can
+			// teleport back to the position they teleported from.
+			if (skill.Owner.IsAbilityActive(AbilityId.Wizard30))
+				result = 2;
+
+			return result;
 		}
 	}
 }
