15a008983c9fd4947399e210317a9c405bc50840
Exec Tails <exec@dragonbyte.de>
2026-02-26 15:43:42 +0100

    Minor comment improvement

--- system/scripts/zone/core/calc_skill_override.cs
+++ system/scripts/zone/core/calc_skill_override.cs
@@ -1,14 +1,22 @@
 ﻿//--- Melia Script ----------------------------------------------------------
 // Skill Calculation Script
 //--- Description -----------------------------------------------------------
-// Functions that calculate skill-related values, such as properties.
+// Functions that calculate skill-related values, such as properties,
+// and override general purpose functions for specific skills.
+//--- Notes -----------------------------------------------------------------
+// Skill calculation functions automatically switch to override functions
+// if they exist. For example, if the property SpendSP is calculated, set
+// up as a calculated property that is to use the function
+// "SCR_Get_SpendSP", that function will be used by default. However, if
+// the skill's class name is "Wizard_EarthQuake", and a function named
+// "SCR_Get_SpendSP_Wizard_EarthQuake" exists, that function will be used
+// instead of the default one.
 //---------------------------------------------------------------------------
 
 using Melia.Shared.Game.Const;
 using Melia.Zone.Scripting;
 using Melia.Zone.Skills;
 using Melia.Zone.World.Actors;
-using Melia.Zone.World.Actors.Characters.Components;
 using Melia.Zone.World.Actors.CombatEntities.Components;
 
 public class SkillOverrideCalculationsScript : GeneralScript
