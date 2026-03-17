a7798c86234b13eabe393f55aa2d41f0fc6fdff3
Exec Tails <exec@dragonbyte.de>
2026-03-17 01:23:29 +0100

    Improved no advancement client script
    Added hiding of advance button in advancement info.

--- system/scripts/zone/core/client/no_advancement/001.lua
+++ system/scripts/zone/core/client/no_advancement/001.lua
@@ -1,3 +1,10 @@
 Melia.Override("ON_CHANGE_JOB_BUTTON", function(original, frame, msg, name, range)
-	frame:ShowWindow(0);
+	frame:ShowWindow(0)
+end)
+
+Melia.Override("CHANGEJOB_OPEN", function(original, frame)
+	original(frame)
+
+	local class_select = GET_CHILD_RECURSIVELY(frame, "class_select")
+	class_select:ShowWindow(0)
 end)
