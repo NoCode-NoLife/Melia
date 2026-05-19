67e93eeff654cc5c3adef3ad6854fb0ddba85b61
Exec Tails <exec@dragonbyte.de>
2026-05-19 08:48:28 +0200

    Updated ops to i404215

--- src/Shared/Network/Op.cs
+++ src/Shared/Network/Op.cs
@@ -1230,6 +1230,7 @@ namespace Melia.Shared.Network
 		public const int CZ_REQ_GUILD_QUEST_REWARD_INFO = 0x1084; // Size: 26
 		public const int ZC_SEND_GUILD_QUEST_REWARD_INFO = 0x1085; // Size: 0
 		public const int CZ_CHECK_GUILD_TICKET_RESET = 0x1086; // Size: 26
+		public const int ZC_COOLDOWN_CHANGED_BATCH = 0x1087; // Size: 0
 		public const int CS_LOGIN = 0x3E1D; // Size: 411
 		public const int SC_NORMAL = 0x3E1E; // Size: 0
 		public const int SC_FROM_INTEGRATE = 0x3E1F; // Size: 0
@@ -2494,6 +2495,7 @@ namespace Melia.Shared.Network
 			Sizes[CZ_REQ_GUILD_QUEST_REWARD_INFO] = 26;
 			Sizes[ZC_SEND_GUILD_QUEST_REWARD_INFO] = 0;
 			Sizes[CZ_CHECK_GUILD_TICKET_RESET] = 26;
+			Sizes[ZC_COOLDOWN_CHANGED_BATCH] = 0;
 			Sizes[CS_LOGIN] = 411;
 			Sizes[SC_NORMAL] = 0;
 			Sizes[SC_FROM_INTEGRATE] = 0;
